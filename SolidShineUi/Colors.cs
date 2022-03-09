c2aafad86864516647352a89bb61025d44388929
JaykeBird <jacobrh94@hotmail.com>
2022-03-09 14:39:23 -0600

    Add Color blend function
    Finding more functions to add into ColorsHelper. I plan on adding more functions later... once I think of what functions to add lol.

--- SolidShineUi/Colors.cs
+++ SolidShineUi/Colors.cs
@@ -135,6 +135,25 @@ namespace SolidShineUi
 
         #endregion
 
+        #region Additional Functions
+
+        /// <summary>Blends the specified colors together.</summary>
+        /// <param name="color">Color to blend onto the background color.</param>
+        /// <param name="backColor">Color to blend the other color onto.</param>
+        /// <param name="amount">How much of <paramref name="color"/> to keep,
+        /// “on top of” <paramref name="backColor"/>.</param>
+        /// <returns>The blended colors.</returns>
+        public static Color Blend(Color color, Color backColor, double amount)
+        {
+            // TODO: make sure amount is between 0 and 1
+            byte r = (byte)(color.R * amount + backColor.R * (amount));
+            byte g = (byte)(color.G * amount + backColor.G * (amount));
+            byte b = (byte)(color.B * amount + backColor.B * (amount));
+            return Color.FromRgb(r, g, b);
+        }
+
+        #endregion
+
         #region HSV Math (used for color schemes)
 
         internal static Color AddValue(double h, double s, double v, double add)
