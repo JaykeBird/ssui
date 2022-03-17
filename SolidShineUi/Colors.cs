fb4887b0f0a82084f9374a10851dbaa2637cd186
JaykeBird <jacobrh94@hotmail.com>
2022-03-17 00:27:15 -0500

    Color blend makes sure amount is between 0 and 1
    Added handling in case an amount below 0 or above 1 was passed in.

--- SolidShineUi/Colors.cs
+++ SolidShineUi/Colors.cs
@@ -141,11 +141,13 @@ namespace SolidShineUi
         /// <param name="color">Color to blend onto the background color.</param>
         /// <param name="backColor">Color to blend the other color onto.</param>
         /// <param name="amount">How much of <paramref name="color"/> to keep,
-        /// “on top of” <paramref name="backColor"/>.</param>
+        /// “on top of” <paramref name="backColor"/>. Must be between 0 and 1.</param>
         /// <returns>The blended colors.</returns>
         public static Color Blend(Color color, Color backColor, double amount)
         {
-            // TODO: make sure amount is between 0 and 1
+            if (amount < 0) amount = 0;
+            else if (amount > 1) amount = 1;
+
             byte r = (byte)(color.R * amount + backColor.R * (amount));
             byte g = (byte)(color.G * amount + backColor.G * (amount));
             byte b = (byte)(color.B * amount + backColor.B * (amount));
