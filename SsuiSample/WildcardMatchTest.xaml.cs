f96e3690deb2bbff5e78a8b157e07364ce8b5710
JaykeBird <jacobrh94@hotmail.com>
2022-05-28 14:57:47 -0500

    Add WildcardMatch test
    Add WildcardMatch test to the TestWindow. This allows me to test out the WildcardMatch class.

---
+++ SsuiSample/WildcardMatchTest.xaml.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using SolidShineUi;
+
+namespace SsuiSample
+{
+    /// <summary>
+    /// Interaction logic for WildcardMatchTest.xaml
+    /// </summary>
+    public partial class WildcardMatchTest : UserControl
+    {
+        public WildcardMatchTest()
+        {
+            InitializeComponent();
+        }
+
+        #region ColorScheme
+
+        public event DependencyPropertyChangedEventHandler ColorSchemeChanged;
+
+        public static DependencyProperty ColorSchemeProperty
+            = DependencyProperty.Register("ColorScheme", typeof(ColorScheme), typeof(WildcardMatchTest),
+            new FrameworkPropertyMetadata(new ColorScheme(), new PropertyChangedCallback(OnColorSchemeChanged)));
+
+        public static void OnColorSchemeChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            ColorScheme cs = e.NewValue as ColorScheme;
+
+            if (d is WildcardMatchTest s)
+            {
+                s.ColorSchemeChanged?.Invoke(d, e);
+                s.ApplyColorScheme(cs);
+            }
+        }
+
+        public ColorScheme ColorScheme
+        {
+            get => (ColorScheme)GetValue(ColorSchemeProperty);
+            set => SetValue(ColorSchemeProperty, value);
+        }
+
+        public void ApplyColorScheme(ColorScheme cs)
+        {
+            if (cs != ColorScheme)
+            {
+                ColorScheme = cs;
+                return;
+            }
+        }
+
+        #endregion
+
+        public void RunWildcard()
+        {
+            foreach (SelectableItem item in selList.Items.OfType<SelectableItem>())
+            {
+                if (WildcardMatch.MatchesWildcard(item.Text, txtMatch.Text, chkCase.IsChecked))
+                {
+                    item.RightText = "matches";
+                }
+                else
+                {
+                    item.RightText = "no match";
+                }
+            }
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            StringInputDialog sid = new StringInputDialog();
+            sid.Owner = Window.GetWindow(this);
+            sid.Title = "Wildcard Match";
+            sid.ColorScheme = ColorScheme;
+            sid.WindowStartupLocation = WindowStartupLocation.CenterOwner;
+            sid.Description = "The text to match against:";
+
+            sid.ShowDialog();
+
+            if (sid.DialogResult)
+            {
+                SelectableItem si = new SelectableItem();
+                si.Text = sid.Value;
+                selList.Items.Add(si);
+            }
+        }
+
+        private void btnRemove_Click(object sender, RoutedEventArgs e)
+        {
+            selList.RemoveSelectedItems();
+        }
+
+        private void btnRemoveAll_Click(object sender, RoutedEventArgs e)
+        {
+            selList.Items.Clear();
+        }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (SelectableItem item in selList.Items.OfType<SelectableItem>())
+            {
+                item.RightText = "";
+            }
+        }
+
+        private void btnDeselect_Click(object sender, RoutedEventArgs e)
+        {
+            selList.Items.ClearSelection();
+        }
+
+        private void btnMatch_Click(object sender, RoutedEventArgs e)
+        {
+            RunWildcard();
+        }
+    }
+}
