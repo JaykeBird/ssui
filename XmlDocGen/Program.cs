b6a463675abc87a8a22a69848035d6f679cd5d89
JaykeBird <jacobrh94@hotmail.com>
2022-05-29 21:57:11 -0500

    Added documentation generator (XmlDocMarkdown)
    I had to follow the instructions included with https://github.com/ejball/XmlDocMarkdown/issues/69 to get this to work properly, but it did!
    
    Next, now that I have the generated Markdown files, I'll upload its contents to GitHub's wiki.

---
+++ XmlDocGen/Program.cs
@@ -0,0 +1,21 @@
+﻿using XmlDocMarkdown.Core;
+
+namespace SolidShineUi.XmlDocGen
+{
+    public class Program
+    {
+        public static int Main(string[] args)
+        {
+            return XmlDocMarkdownApp.Run(args);
+
+            //if (args.Length == 0)
+            //{
+            //    return XmlDocMarkdownApp.Run(new List<string>{"SolidShineUi.dll", "docs"});
+            //}
+            //else
+            //{
+            //    return XmlDocMarkdownApp.Run(args);
+            //}
+        }
+    }
+}
