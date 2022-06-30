e835f248d8b399dede74ad71bad47fe067d17917
JaykeBird <jacobrh94@hotmail.com>
2022-06-29 23:28:57 -0500

    Add error handling to DoubleStringConverter
    Not sure why this class is still here, as I don't think I use it anywhere, but whatever lol! I wonder how it ended up here in the first place...
    
    Anyway, added handling in case the parameter passed in was not an integer (or couldn't be casted to an integer).

--- SolidShineUi/Utils/DoubleStringConverter.cs
+++ SolidShineUi/Utils/DoubleStringConverter.cs
@@ -9,13 +9,32 @@ namespace SolidShineUi.Utils
     /// </summary>
     public class DoubleStringConverter : IValueConverter
     {
+
+        /// <summary>
+        /// Convert a double into a string, with rounding possible by setting the <paramref name="parameter"/> value.
+        /// </summary>
+        /// <param name="value">The double object to convert</param>
+        /// <param name="targetType">Not used, returned type will always be string</param>
+        /// <param name="parameter">The number of decimals to use (integer)</param>
+        /// <param name="culture">Not used</param>
+        /// <returns></returns>
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            int Decimals = (int)parameter;
-            return Math.Round((double)value, Decimals).ToString();
+            try
+            {
+                int Decimals = (int)parameter;
+                return Math.Round((double)value, Decimals).ToString();
+            }
+            catch (InvalidCastException)
+            {
+                return (value ?? 0.0d).ToString() ?? "0.0";
+            }
             //throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Convert a string back into a double, by parsing the string.
+        /// </summary>
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
             try
