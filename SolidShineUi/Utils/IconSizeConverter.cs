e18159d94039611e61a2ad3bf50faa911a7d1637
JaykeBird <jacobrh94@hotmail.com>
2023-01-24 08:31:40 -0600

    Display 16x16 icon in FlatWindow
    Used and expanded upon a converter I found on StackOverflow; when you provide it a size value, it'll attempt to load an icon of that size (or the nearest size available).
    
    Now the top-left icon displayed in FlatWindow will be the 16x16 one, rather than the 32x32 one or a different one like before.

---
+++ SolidShineUi/Utils/IconSizeConverter.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows;
+using System.Windows.Markup;
+using System.Windows.Media.Imaging;
+using System.Diagnostics;
+using System.Windows.Media;
+
+namespace SolidShineUi.Utils
+{
+    // from https://stackoverflow.com/a/54738646/2987285
+
+    /// <summary>
+    /// Forces the selection of a given size from the ICO file/resource. 
+    /// If the exact size does not exists, selects the closest smaller if possible otherwise closest higher resolution.
+    /// If no parameter is given, the smallest frame available will be selected
+    /// </summary>
+    public class IconSizeConverter : IValueConverter
+    {
+        /// <inheritdoc/>
+        public virtual object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+#if NETCOREAPP
+            Uri? uri = null;
+#else
+            Uri uri = null;
+#endif
+
+            int size = string.IsNullOrWhiteSpace(parameter?.ToString()) ? 0 : System.Convert.ToInt32(parameter);
+
+            if (value is BitmapFrame bfd)
+            {
+                // a future efficiency thing would be to reuse this existing decoder, rather than creating our own later
+                uri = new Uri(bfd.Decoder.ToString());
+            }
+            else if (value is BitmapImage bmp)
+            {
+                if (!string.IsNullOrWhiteSpace(bmp.UriSource.ToString()))
+                {
+                    uri = bmp.UriSource;
+                }
+            }
+            else
+            {
+                string suri = value?.ToString()?.Trim();
+
+                if (string.IsNullOrWhiteSpace(suri))
+                {
+                    return null;
+                }
+                else if (Uri.IsWellFormedUriString(suri, UriKind.Absolute))
+                {
+                    return suri;
+                }
+                else if (!suri.StartsWith("pack:"))
+                {
+                    suri = $"pack://application:,,,{suri}";
+                }
+
+                uri = new Uri(suri);
+            }
+
+            var decoder = BitmapDecoder.Create(uri, BitmapCreateOptions.DelayCreation, BitmapCacheOption.OnDemand);
+
+            //Debug.WriteLine("DECODER FRAMES: " + decoder.Frames);
+
+#if NETCOREAPP
+            BitmapFrame? result = decoder.Frames.Where(f => f.Width <= size).OrderByDescending(f => f.Width).FirstOrDefault()
+                ?? decoder.Frames.OrderBy(f => f.Width).FirstOrDefault();
+#else
+            BitmapFrame result = decoder.Frames.Where(f => f.Width <= size).OrderByDescending(f => f.Width).FirstOrDefault()
+                ?? decoder.Frames.OrderBy(f => f.Width).FirstOrDefault();
+#endif
+
+            //Debug.WriteLine("SELECTED FRAME: " + result?.Width ?? "(null)");
+
+            return result;
+        }
+
+        /// <inheritdoc/>
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
