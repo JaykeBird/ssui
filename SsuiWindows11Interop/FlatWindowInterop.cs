f787de2fa3d8e4a82369ba97092c4c6f05d3be73
JaykeBird <jacobrh94@hotmail.com>
2023-01-26 09:20:12 -0600

    Get Windows11Interop to work lol
    Figured out what was needed to get this work, so yay! There's some other issues that need attention, but at least now the code is present for those who want it.
    
    I'll need to figure out a way to test it...

---
+++ SsuiWindows11Interop/FlatWindowInterop.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.UI.Windowing;
+using System.Windows.Interop;
+
+namespace SsuiWindows11Interop
+{
+    public static class FlatWindowInterop
+    {
+
+        public static void RenderWindowsCaptionButtons(ref FlatWindow w)
+        {
+            if (!AppWindowTitleBar.IsCustomizationSupported()) return;
+
+            WindowInteropHelper wih = new WindowInteropHelper(w);
+            IntPtr hwnd = wih.EnsureHandle();
+
+            // Retrieve the WindowId that corresponds to hWnd.
+            Microsoft.UI.WindowId wid = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(hwnd);
+            AppWindow appWindow = AppWindow.GetFromWindowId(wid);
+
+            appWindow.TitleBar.ExtendsContentIntoTitleBar = true;
+            w.CaptionDisplayType = ChromeButtons.CaptionType.None;
+        }
+
+    }
+}
