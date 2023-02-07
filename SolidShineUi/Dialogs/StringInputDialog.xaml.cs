23652009c9028ed59ac10969d394560852258379
JaykeBird <jacobrh94@hotmail.com>
2023-02-07 14:28:43 -0600

    Add validation check features to StringInputDialog
    If you provide a validation function, the StringInputDialog can use this to check the string and prevent the OK button from being clicked unless the string matches an expected format.
    
    As a part of this, you can display an optional validation success and validation failure message, to further explain to the user the current state or problem.

--- SolidShineUi/Dialogs/StringInputDialog.xaml.cs
+++ SolidShineUi/Dialogs/StringInputDialog.xaml.cs
@@ -80,12 +80,26 @@ namespace SolidShineUi
         /// <summary>
         /// Get or set whether the Enter key can be used to confirm the dialog. If enabled, pressing down the Enter key will be treated as if the user pressed "OK".
         /// </summary>
-        public bool EnterKeyConfirms { get; set; } = true;
+        public bool EnterKeyConfirms { get => (bool)GetValue(EnterKeyConfirmsProperty); set => SetValue(EnterKeyConfirmsProperty, value); }
+
+        /// <summary>
+        /// A dependency property backing the related property. Please see <see cref="EnterKeyConfirms"/> for details.
+        /// </summary>
+        public static DependencyProperty EnterKeyConfirmsProperty
+            = DependencyProperty.Register("EnterKeyConfirms", typeof(bool), typeof(StringInputDialog),
+            new FrameworkPropertyMetadata(true));
 
         /// <summary>
         /// Get or set whether the Escape key can be used to cancel the dialog. If enabled, pressing down the Escape key will be treated as if the user pressed "Cancel".
         /// </summary>
-        public bool EscapeKeyCancels { get; set; } = true;
+        public bool EscapeKeyCancels { get => (bool)GetValue(EscapeKeyCancelsProperty); set => SetValue(EscapeKeyCancelsProperty, value); }
+
+        /// <summary>
+        /// A dependency property backing the related property. Please see <see cref="EscapeKeyCancels"/> for details.
+        /// </summary>
+        public static DependencyProperty EscapeKeyCancelsProperty
+            = DependencyProperty.Register("EscapeKeyCancels", typeof(bool), typeof(StringInputDialog),
+            new FrameworkPropertyMetadata(true));
 
         /// <summary>
         /// Get or set whether all of the text in the text box should be selected when the text box receives focus.
@@ -102,17 +116,82 @@ namespace SolidShineUi
         /// <remarks>Try to keep the description to about a sentence long. If you do have a lengthier description, you may need to resize the window to make it fit properly.
         /// Ideally, the overall design of the program should make it apparent what the user should enter into the text box without reading the description.
         /// However, the description is helpful to remind the user what is being asked of them here, and also to potentially clarify the types of values that are valid or invalid.</remarks>
-        public string Description
+        public string Description { get => (string)GetValue(DescriptionProperty); set => SetValue(DescriptionProperty, value); }
+
+        /// <summary>
+        /// A dependency property backing the related property. See <see cref="Description"/> for details.
+        /// </summary>
+        public static DependencyProperty DescriptionProperty
+            = DependencyProperty.Register("Description", typeof(string), typeof(StringInputDialog),
+            new FrameworkPropertyMetadata("Enter a value:"));
+
+        #region Data Validation
+
+        /// <summary>
+        /// Get or set the data validation function that should be used to make sure the inputted string matches an expected format.
+        /// </summary>
+        /// <remarks>
+        /// Set this to <c>null</c> if you want to disable data validation.
+        /// This function is called every time that the text in the text box is changed, so it's important to have a function that is relatively fast, so as
+        /// to avoid blocking the UI thread.
+        /// The function will be given the current text in the text box, and is asked to return a bool if it matches the expected validation
+        /// (<c>true</c> for a good match, <c>false</c> for an invalid string).
+        /// </remarks>
+#if NETCOREAPP
+        public Func<string, bool>? ValidationFunction { get; set; } = null;
+#else
+        public Func<string, bool> ValidationFunction { get; set; } = null;
+#endif
+
+        /// <summary>
+        /// Get or set the string to display in the UI when the validation returns true (a success), indicating a good and valid string.
+        /// </summary>
+        public string ValidationSuccessString { get => (string)GetValue(ValidationSuccessStringProperty); set => SetValue(ValidationSuccessStringProperty, value); }
+
+        /// <summary>
+        /// A dependency property backing the related property. See <see cref="ValidationSuccessString"/> for details.
+        /// </summary>
+        public static DependencyProperty ValidationSuccessStringProperty
+            = DependencyProperty.Register("ValidationSuccessString", typeof(string), typeof(StringInputDialog),
+            new FrameworkPropertyMetadata(""));
+
+        /// <summary>
+        /// Get or set the string to display in the UI when the validation returns false (a failure), indicating an invalid or unusable string.
+        /// </summary>
+        public string ValidationFailureString { get => (string)GetValue(ValidationFailureStringProperty); set => SetValue(ValidationFailureStringProperty, value); }
+
+        /// <summary>
+        /// A dependency property backing the related property. See <see cref="ValidationFailureString"/> for details.
+        /// </summary>
+        public static DependencyProperty ValidationFailureStringProperty
+            = DependencyProperty.Register("ValidationFailureString", typeof(string), typeof(StringInputDialog),
+            new FrameworkPropertyMetadata("Input does not match expected result"));
+
+        void CheckValidation()
         {
-            get
+            if (ValidationFunction != null)
             {
-                return txtDesc.Text;
+                bool result = ValidationFunction(txtValue.Text);
+                if (result)
+                {
+                    txtValidation.Text = ValidationSuccessString;
+                    btnOK.IsEnabled = true;
+                }
+                else
+                {
+                    txtValidation.Text = ValidationFailureString;
+                    btnOK.IsEnabled = false;
+                }
             }
-            set
+            else
             {
-                txtDesc.Text = value;
+                // if validation is disabled, make sure the OK button is good to be used
+                txtValidation.Text = "";
+                btnOK.IsEnabled = true;
             }
         }
+
+        #endregion
 
         private void btnOK_Click(object sender, RoutedEventArgs e)
         {
@@ -125,7 +204,7 @@ namespace SolidShineUi
             Close();
         }
 
-        private void txtName_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        private void txtValue_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
         {
             if (e.Key == System.Windows.Input.Key.Enter && EnterKeyConfirms)
             {
@@ -135,6 +214,11 @@ namespace SolidShineUi
             {
                 btnCancel_Click(this, e);
             }
+        }
+
+        private void txtValue_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            CheckValidation();
         }
     }
 }
