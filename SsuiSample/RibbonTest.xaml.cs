6219792ebcf1e23f5f7b4576bcd41d130f7b069f
JaykeBird <jacobrh94@hotmail.com>
2023-05-25 21:52:44 -0500

    Set up RibbonTest pane in SsuiSample
    Set up the RibbonTest pane to display a RibbonGroup and... allow testing it. Once I have the full Ribbon control up and running, I'll use this pane for displaying that instead. For now, this confirms that RibbonGroup is working, as well as RibbonButton and RibbonMenuButton.
    
    Also added some icons to use with the buttons!

---
+++ SsuiSample/RibbonTest.xaml.cs
@@ -0,0 +1,95 @@
+﻿using SolidShineUi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SsuiSample
+{
+    /// <summary>
+    /// Interaction logic for RibbonTest.xaml
+    /// </summary>
+    public partial class RibbonTest : UserControl
+    {
+        public RibbonTest()
+        {
+            InitializeComponent();
+        }
+
+
+        #region Color Scheme
+        /// <summary>
+        /// Raised when the ColorScheme property is changed.
+        /// </summary>
+#if NETCOREAPP
+        public event DependencyPropertyChangedEventHandler? ColorSchemeChanged;
+#else
+        public event DependencyPropertyChangedEventHandler ColorSchemeChanged;
+#endif
+
+
+        /// <summary>
+        /// A dependency property object backing the related ColorScheme property. See <see cref="ColorScheme"/> for more details.
+        /// </summary>
+        public static readonly DependencyProperty ColorSchemeProperty
+            = DependencyProperty.Register("ColorScheme", typeof(ColorScheme), typeof(RibbonTest),
+            new FrameworkPropertyMetadata(new ColorScheme(), new PropertyChangedCallback(OnColorSchemeChanged)));
+
+        /// <summary>
+        /// Perform an action when the ColorScheme property has changed. Primarily used internally.
+        /// </summary>
+        /// <param name="d">The object containing the property that changed.</param>
+        /// <param name="e">Event arguments about the property change.</param>
+        public static void OnColorSchemeChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+#if NETCOREAPP
+            ColorScheme cs = (e.NewValue as ColorScheme)!;
+#else
+            ColorScheme cs = e.NewValue as ColorScheme;
+#endif
+
+            if (d is RibbonTest c)
+            {
+                c.ColorSchemeChanged?.Invoke(d, e);
+                c.ApplyColorScheme(cs);
+            }
+        }
+
+        /// <summary>
+        /// Get or set the color scheme used for this control. The color scheme can quickly apply a whole visual style to your control.
+        /// </summary>
+        public ColorScheme ColorScheme
+        {
+            get => (ColorScheme)GetValue(ColorSchemeProperty);
+            set => SetValue(ColorSchemeProperty, value);
+        }
+
+        /// <summary>
+        /// Apply a color scheme to this control. The color scheme can quickly apply a whole visual style to the control.
+        /// </summary>
+        /// <param name="cs">The color scheme to apply.</param>
+        public void ApplyColorScheme(ColorScheme cs)
+        {
+            if (cs == null)
+            {
+                return;
+            }
+            if (cs != ColorScheme)
+            {
+                ColorScheme = cs;
+                return;
+            }
+        }
+        #endregion
+    }
+}
