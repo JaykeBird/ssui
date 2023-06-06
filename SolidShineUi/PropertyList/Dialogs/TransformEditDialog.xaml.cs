f1126dc9bedca796b555a1a33f40b62ed340129a
JaykeBird <jacobrh94@hotmail.com>
2023-06-06 01:25:40 -0500

    Starting work on Transform edit dialog
    This dialog will eventually allow users to edit the Transform and RelativeTransform properties of various objects.
    
    There is no logic here, and only the beginning of the UI design, but hopefully from here you can start to see what my main idea is here.
    
    I'll construct more in a future commit.

---
+++ SolidShineUi/PropertyList/Dialogs/TransformEditDialog.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace SolidShineUi.PropertyList.Dialogs
+{
+    /// <summary>
+    /// Interaction logic for TransformEditDialog.xaml
+    /// </summary>
+    public partial class TransformEditDialog : FlatWindow
+    {
+        public TransformEditDialog()
+        {
+            InitializeComponent();
+        }
+    }
+}
