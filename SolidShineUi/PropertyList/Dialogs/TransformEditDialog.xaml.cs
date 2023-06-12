fdb5a47a9779fb4a6e7d1d0043d972d59e5b45ba
JaykeBird <jacobrh94@hotmail.com>
2023-06-12 09:25:36 -0500

    Begin hooking up editors in TransformEditDialog
    Added in the core functions needed for interacting with the editors in the dialog. Right now, this still doesn't load or save anything to the items on the left pane, but this is another step closer!

--- SolidShineUi/PropertyList/Dialogs/TransformEditDialog.xaml.cs
+++ SolidShineUi/PropertyList/Dialogs/TransformEditDialog.xaml.cs
@@ -46,12 +46,16 @@ namespace SolidShineUi.PropertyList.Dialogs
         public SelectableCollection<TransformSelectableControl> TransformList { 
             get => (SelectableCollection<TransformSelectableControl>)GetValue(TransformListProperty); set => SetValue(TransformListProperty, value); }
 
+        /// <summary>
+        /// The backing dependency property object for <see cref="TransformList"/>. See the related property for more details.
+        /// </summary>
         public static DependencyProperty TransformListProperty
             = DependencyProperty.Register("TransformList", typeof(SelectableCollection<TransformSelectableControl>), typeof(TransformEditDialog),
             new FrameworkPropertyMetadata(new SelectableCollection<TransformSelectableControl>()));
 
         private void TransformList_SelectionChanged(object sender, CollectionSelectionChangedEventArgs e)
         {
+
             btnDelete.IsEnabled = TransformList.SelectedItems.Count > 0;
             btnMoveDown.IsEnabled = TransformList.SelectedItems.Count > 0;
             btnMoveUp.IsEnabled = TransformList.SelectedItems.Count > 0;
@@ -78,26 +82,31 @@ namespace SolidShineUi.PropertyList.Dialogs
                 {
                     // load rotation controls
                     DisplayTransformControl(grdRotate);
+                    LoadInRotateTransform(r);
                 }
                 else if (selTransform is ScaleTransform s)
                 {
                     // load scale controls
                     DisplayTransformControl(grdScale);
+                    LoadInScaleTransform(s);
                 }
                 else if (selTransform is TranslateTransform u)
                 {
                     // load translation controls
                     DisplayTransformControl(grdTranslate);
+                    LoadInTranslateTransform(u);
                 }
                 else if (selTransform is SkewTransform k)
                 {
                     // load skew controls
                     DisplayTransformControl(grdSkew);
+                    LoadInSkewTransform(k);
                 }
                 else if (selTransform is MatrixTransform m)
                 {
                     // load matrix controls
                     DisplayTransformControl(grdMatrix);
+                    LoadInMatrixTransform(m);
                 }
                 else if (selTransform is TransformGroup g)
                 {
@@ -108,6 +117,7 @@ namespace SolidShineUi.PropertyList.Dialogs
             
         }
 
+        #region Transform Editors
 #if NETCOREAPP
         void DisplayTransformControl(Grid? transformControl)
 #else
@@ -145,6 +155,93 @@ namespace SolidShineUi.PropertyList.Dialogs
                 //txtGridEmpty.Visibility = Visibility.Collapsed;
             }
         }
+
+        Transform GetValuesFromActiveTransform()
+        {
+            UIElement visibleElement = txtGridEmpty;
+            foreach (UIElement item in grdHolder.Children)
+            {
+                if (item.Visibility == Visibility.Visible)
+                {
+                    visibleElement = item;
+                }
+            }
+
+            if (visibleElement == txtGridEmpty) { return new MatrixTransform(Matrix.Identity); }
+            else if (visibleElement == grdMatrix) { return GetValuesMatrixTransform(); }
+            else if (visibleElement == grdRotate) { return GetValuesRotateTransform(); }
+            else if (visibleElement == grdScale) { return GetValuesScaleTransform(); }
+            else if (visibleElement == grdSkew) { return GetValuesSkewTransform(); }
+            else if (visibleElement == grdTranslate) { return GetValuesTranslateTransform(); }
+            else { return new MatrixTransform(Matrix.Identity); }
+        }
+
+        void LoadInRotateTransform(RotateTransform r)
+        {
+            nudRotateAngle.Value = r.Angle;
+            nudRotateCenterX.Value = r.CenterX;
+            nudRotateCenterY.Value = r.CenterY;
+        }
+
+        RotateTransform GetValuesRotateTransform()
+        {
+            return new RotateTransform(nudRotateAngle.Value, nudRotateCenterX.Value, nudRotateCenterY.Value);
+        }
+
+        void LoadInScaleTransform(ScaleTransform s)
+        {
+            nudScaleX.Value = s.ScaleX;
+            nudScaleY.Value = s.ScaleY;
+            nudScaleCenterX.Value = s.CenterX;
+            nudScaleCenterY.Value = s.CenterY;
+        }
+
+        ScaleTransform GetValuesScaleTransform()
+        {
+            return new ScaleTransform(nudScaleX.Value, nudScaleY.Value, nudScaleCenterX.Value, nudScaleCenterY.Value);
+        }
+
+        void LoadInTranslateTransform(TranslateTransform t)
+        {
+            nudTranslateX.Value = t.X;
+            nudTranslateY.Value = t.Y;
+        }
+
+        TranslateTransform GetValuesTranslateTransform()
+        {
+            return new TranslateTransform(nudTranslateX.Value, nudTranslateY.Value);
+        }
+
+        void LoadInSkewTransform(SkewTransform s)
+        {
+            nudSkewX.Value = s.AngleX;
+            nudSkewY.Value = s.AngleY;
+            nudSkewCenterX.Value = s.CenterX;
+            nudSkewCenterY.Value = s.CenterY;
+        }
+
+        SkewTransform GetValuesSkewTransform()
+        {
+            return new SkewTransform(nudSkewX.Value, nudSkewY.Value, nudSkewCenterX.Value, nudSkewCenterY.Value);
+        }
+
+        void LoadInMatrixTransform(MatrixTransform m)
+        {
+            nudMatrix11.Value = m.Matrix.M11;
+            nudMatrix12.Value = m.Matrix.M12;
+            nudMatrix21.Value = m.Matrix.M21;
+            nudMatrix22.Value = m.Matrix.M22;
+            nudMatrixOffsetX.Value = m.Matrix.OffsetX;
+            nudMatrixOffsetY.Value = m.Matrix.OffsetY;
+        }
+
+        MatrixTransform GetValuesMatrixTransform()
+        {
+            return new MatrixTransform(nudMatrix11.Value, nudMatrix12.Value, nudMatrix21.Value, nudMatrix22.Value,
+                                            nudMatrixOffsetX.Value, nudMatrixOffsetY.Value);
+        }
+
+        #endregion
 
         #region Add menu
         private void mnuRotateAdd_Click(object sender, RoutedEventArgs e)
@@ -203,6 +300,74 @@ namespace SolidShineUi.PropertyList.Dialogs
             DialogResult = false;
             Close();
         }
+
+        #region Editor Reset Buttons / Matrix Additions Menu
+        private void btnRotateReset_Click(object sender, RoutedEventArgs e)
+        {
+            nudRotateCenterX.Value = 0;
+            nudRotateCenterY.Value = 0;
+        }
+
+        private void btnScaleReset_Click(object sender, RoutedEventArgs e)
+        {
+            nudScaleCenterX.Value = 0;
+            nudScaleCenterY.Value = 0;
+        }
+
+        private void btnSkewReset_Click(object sender, RoutedEventArgs e)
+        {
+            nudSkewCenterX.Value = 0;
+            nudSkewCenterY.Value = 0;
+        }
+
+        private void btnTranslateReset_Click(object sender, RoutedEventArgs e)
+        {
+            nudTranslateX.Value = 0;
+            nudTranslateY.Value = 0;
+        }
+
+        private void mnuMatrixRotateAppend_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void mnuMatrixScaleAppend_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void mnuMatrixTranslateAppend_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void mnuMatrixRotatePrepend_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void mnuMatrixScalePrepend_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void mnuMatrixTranslatePrepend_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnMatrixReset_Click(object sender, RoutedEventArgs e)
+        {
+            Matrix m = Matrix.Identity;
+
+            nudMatrix11.Value = m.M11;
+            nudMatrix12.Value = m.M12;
+            nudMatrix21.Value = m.M21;
+            nudMatrix22.Value = m.M22;
+            nudMatrixOffsetX.Value = m.OffsetX;
+            nudMatrixOffsetY.Value = m.OffsetY;
+        }
+        #endregion
     }
 
     public class TransformSelectableControl : SelectableItem
