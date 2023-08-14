2884f095128990406597f365f6f79c08ca7ee537
JaykeBird <jacobrh94@hotmail.com>
2023-08-14 16:18:59 -0500

    Create derived ItemsControl to use in RibbonGroup
    Created a modified version of the ItemsControl that allows me to modify how items are rendered on the RibbonGroup, beyond what was doable before. With this, I was able to make it so narrower buttons didn't take up the full width of a column when not needed.
    
    Ideally with only this one small little change, everything else should continue to act as normal. But over time, we'll see if there's any knock-on effects I need to be wary of.

---
+++ SolidShineUi/Toolbars/Ribbon/Utils/RibbonGroupItemsControl.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace SolidShineUi.Toolbars.Ribbon.Utils
+{
+    public class RibbonGroupItemsControl : ItemsControl
+    {
+
+        /// <inheritdoc/>
+        protected override bool IsItemItsOwnContainerOverride(object item)
+        {
+            return false;
+            //return base.IsItemItsOwnContainerOverride(item);
+        }
+
+    }
+}
