f33c7afa005aa6d69f981bef339100a4aef65d6f
JaykeBird <jacobrh94@hotmail.com>
2023-10-29 22:55:56 -0500

    Remove "public" from IClickSelectableControl items
    I fall too much into the habit of labeling everything "public" when in interfaces, that isn't needed lol. Whoops!

--- SolidShineUi/IClickSelectableControl.cs
+++ SolidShineUi/IClickSelectableControl.cs
@@ -16,46 +16,46 @@ namespace SolidShineUi
         /// <summary>
         /// Get or set the selection status of this control. Selected controls often have a different visual appearance as well.
         /// </summary>
-        public bool IsSelected { get; set; }
+        bool IsSelected { get; set; }
 
         /// <summary>
         /// Get or set if this control's <see cref="IsSelected"/> value changes when it is clicked.
         /// </summary>
-        public bool SelectOnClick { get; set; }
+        bool SelectOnClick { get; set; }
 
         /// <summary>
         /// Raised when the user clicks on this control.
         /// </summary>
-        public event RoutedEventHandler Click;
+        event RoutedEventHandler Click;
 
         /// <summary>
         /// Raised when the user right-clicks on this control. This usually activates a context menu, but could be used for other actions.
         /// </summary>
-        public event RoutedEventHandler RightClick;
+        event RoutedEventHandler RightClick;
 
         /// <summary>
         /// Perform a click programmatically. This control responds the same way as if it was clicked by the user.
         /// </summary>
-        public void DoClick();
+        void DoClick();
 
         /// <summary>
         /// Get or set the background brush used for this control, when it isn't being clicked or selected.
         /// </summary>
-        public Brush Background { get; set; }
+        Brush Background { get; set; }
 
         /// <summary>
         /// Get or set the background brush used for this control, while it has the mouse over it or has keyboard focus.
         /// </summary>
-        public Brush HighlightBrush { get; set; }
+        Brush HighlightBrush { get; set; }
 
         /// <summary>
         /// Get or set the background brush used for this control, while the mouse is clicking down on it.
         /// </summary>
-        public Brush ClickBrush { get; set; }
+        Brush ClickBrush { get; set; }
 
         /// <summary>
         /// Get or set the background brush used for this control, while it is selected (and isn't being clicked).
         /// </summary>
-        public Brush SelectedBrush { get; set; }
+        Brush SelectedBrush { get; set; }
     }
 }
