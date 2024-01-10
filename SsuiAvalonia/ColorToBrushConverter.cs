f5a98bb4f939de0a1f7951dee6c75870c281abc9
JaykeBird <jacobrh94@hotmail.com>
2024-01-09 20:20:34 -0600

    Add ColorToBrushConverter to Avalonia
    Yet another WPF value converter ported over. Will be working on more soon!

---
+++ SsuiAvalonia/ColorToBrushConverter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Globalization;
+using Avalonia.Media;
+using Avalonia.Data.Converters;
+using Avalonia.Data;
+
+namespace SolidShineUi
+{
+    /// <summary>
+    /// Converts a Color into a SolidColorBrush.
+    /// </summary>
+    public class ColorToBrushConverter : IValueConverter
+    {
+        /// <summary>
+        /// Convert a Color into a SolidColorBrush.
+        /// </summary>
+        /// <remarks>
+        /// If the value is not of type <see cref="Color"/>, then a <see cref="BindingNotification"/> in an error state is returned.
+        /// </remarks>
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (value is Color c)
+            {
+                return new SolidColorBrush(c);
+            }
+            else return new BindingNotification(new InvalidCastException("value is not of type Color"), BindingErrorType.Error);
+        }
+
+        /// <summary>
+        /// Convert a SolidColorBrush into a Color.
+        /// </summary>
+        /// <remarks>
+        /// If the value is not of type <see cref="ISolidColorBrush"/>, then a <see cref="BindingNotification"/> in an error state is returned.
+        /// </remarks>
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (value is ISolidColorBrush s)
+            {
+                return s.Color;
+            }
+            else return new BindingNotification(new InvalidCastException("value is not of type ISolidColorBrush"), BindingErrorType.Error);
+        }
+    }
+}
