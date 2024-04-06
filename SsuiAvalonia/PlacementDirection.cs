d97e2285fe42d6f647db59b8758280f64594388e
JaykeBird <jacobrh94@hotmail.com>
2024-04-06 17:41:30 -0500

    Finish code for SplitButton
    The SplitButton is pretty much all done! Now I just need to develop the visuals, which hopefully won't be too hard.

---
+++ SsuiAvalonia/PlacementDirection.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolidShineUi
+{
+    /// <summary>
+    /// Used to indicate the placement of a UI element within a larger parent element/control.
+    /// </summary>
+    public enum PlacementDirection
+    {
+        /// <summary>
+        /// Hide the UI element.
+        /// </summary>
+        Hidden = 0,
+        /// <summary>
+        /// Display the UI element at the top side of the control.
+        /// </summary>
+        Top = 1,
+        /// <summary>
+        /// Display the UI element at the left side of the control.
+        /// </summary>
+        Left = 2,
+        /// <summary>
+        /// Display the UI element at the right side of the control.
+        /// </summary>
+        Right = 3,
+        /// <summary>
+        /// Display the UI element at the bottom side of the control.
+        /// </summary>
+        Bottom = 4,
+    }
+}
--- SsuiAvalonia/SplitButton.cs
+++ SsuiAvalonia/SplitButton.cs
@@ -6,8 +6,6 @@ using Avalonia.Media;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace SolidShineUi
@@ -16,8 +14,15 @@ namespace SolidShineUi
     /// A control that displays two buttons: a main button on the left which activates like a normal button, 
     /// and a secondary button on the right that displays a menu of additional options.
     /// </summary>
-    public class SplitButton : ContentControl//, IClickSelectableControl
+    public class SplitButton : ContentControl, IClickSelectableControl
     {
+        /// <summary>
+        /// Create a SplitButton.
+        /// </summary>
+        public SplitButton()
+        {
+
+        }
 
         #region Appearance
 
@@ -117,6 +122,7 @@ namespace SolidShineUi
                 BorderHighlightBrush = cs.BorderColor.ToBrush();
                 BorderSelectedBrush = cs.BorderColor.ToBrush();
                 BorderDisabledBrush = cs.DarkDisabledColor.ToBrush();
+                SeparatorBrush = cs.BorderColor.ToBrush();
                 DisabledBrush = cs.BackgroundColor.ToBrush();
                 Foreground = cs.ForegroundColor.ToBrush();
                 ClickBrush = cs.ThirdHighlightColor.ToBrush();
@@ -133,6 +139,7 @@ namespace SolidShineUi
                     SelectedBrush = cs.AccentThirdHighlightColor.ToBrush();
                     BorderHighlightBrush = cs.AccentHighlightColor.ToBrush();
                     BorderSelectedBrush = cs.AccentSelectionColor.ToBrush();
+                    SeparatorBrush = cs.AccentBorderColor.ToBrush();
                     Foreground = cs.ForegroundColor.ToBrush();
                     ClickBrush = cs.AccentThirdHighlightColor.ToBrush();
                 }
@@ -146,6 +153,7 @@ namespace SolidShineUi
                     SelectedBrush = cs.ThirdHighlightColor.ToBrush();
                     BorderHighlightBrush = cs.HighlightColor.ToBrush();
                     BorderSelectedBrush = cs.SelectionColor.ToBrush();
+                    SeparatorBrush = cs.BorderColor.ToBrush();
                     Foreground = cs.ForegroundColor.ToBrush();
                     ClickBrush = cs.ThirdHighlightColor.ToBrush();
                 }
@@ -250,6 +258,48 @@ namespace SolidShineUi
         /// <summary>The backing styled property for <see cref="BorderSelectionThickness"/>. See the related property for details.</summary>
         public static readonly StyledProperty<Thickness> BorderSelectionThicknessProperty = FlatButton.BorderSelectionThicknessProperty.AddOwner<SplitButton>();
 
+        #region Separator Border
+
+        /// <summary>
+        /// Get or set if a separator bar should be shown between the main and menu buttons. Without the separator bar, the buttons look more connected, 
+        /// but they can be hard to discern as two separate clickable buttons without mousing over them.
+        /// </summary>
+        public bool ShowSeparator { get => GetValue(ShowSeparatorProperty); set => SetValue(ShowSeparatorProperty, value); }
+
+        /// <summary>The backing styled property for <see cref="ShowSeparator"/>. See the related property for details.</summary>
+        public static readonly StyledProperty<bool> ShowSeparatorProperty
+            = AvaloniaProperty.Register<SplitButton, bool>(nameof(ShowSeparator), true);
+
+        /// <summary>
+        /// Get or set the brush to use for the separator bar between the main and menu buttons.
+        /// </summary>
+        public IBrush? SeparatorBrush { get => GetValue(SeparatorBrushProperty); set => SetValue(SeparatorBrushProperty, value); }
+
+        /// <summary>The backing styled property for <see cref="SeparatorBrush"/>. See the related property for details.</summary>
+        public static readonly StyledProperty<IBrush?> SeparatorBrushProperty
+            = AvaloniaProperty.Register<SplitButton, IBrush?>(nameof(SeparatorBrush), Colors.Gray.ToBrush());
+
+        /// <summary>
+        /// Get or set how distant the separator bar should be from the edges of the button. The higher the number, the bigger the space between
+        /// the edges of the button and the ends of the separator bar. A value of 0 will bring the separator bar right up to the edges.
+        /// </summary>
+        public double SeparatorEdgeMargin { get => GetValue(SeparatorEdgeMarginProperty); set => SetValue(SeparatorEdgeMarginProperty, value); }
+
+        /// <summary>The backing styled property for <see cref="SeparatorEdgeMargin"/>. See the related property for details.</summary>
+        public static readonly StyledProperty<double> SeparatorEdgeMarginProperty
+            = AvaloniaProperty.Register<SplitButton, double>(nameof(SeparatorEdgeMargin), 4.0);
+
+        /// <summary>
+        /// Get or set how wide the separator bar should be. A value of 0 will make it invisible.
+        /// </summary>
+        public double SeparatorThickness { get => GetValue(SeparatorThicknessProperty); set => SetValue(SeparatorThicknessProperty, value); }
+
+        /// <summary>The backing styled property for <see cref="SeparatorThickness"/>. See the related property for details.</summary>
+        public static readonly StyledProperty<double> SeparatorThicknessProperty
+            = AvaloniaProperty.Register<SplitButton, double>(nameof(SeparatorThickness), 1.0);
+
+        #endregion
+
         #endregion
 
         #endregion
@@ -270,7 +320,7 @@ namespace SolidShineUi
         /// </summary>
         public EventHandler? MenuClosed;
 
-        private void Menu_Closing(object? sender, System.ComponentModel.CancelEventArgs e)
+        private void Menu_Closing(object? sender, CancelEventArgs e)
         {
             if (_staysOpen) { e.Cancel = true; }
         }
@@ -353,6 +403,288 @@ namespace SolidShineUi
 
         #endregion
 
+        #region Button Handling
+
+        private void btnMain_Click(object? sender, RoutedEventArgs e)
+        {
+            OnClick();
+        }
+
+        private void btnMenu_Click(object? sender, RoutedEventArgs e)
+        {
+            if (Menu != null)
+            {
+                Menu.Placement = MenuPlacement;
+                Menu.PlacementTarget = MenuPlacementTarget ?? this;
+                Menu.PlacementRect = MenuPlacementRectangle;
+                Menu.HorizontalOffset = 0;
+                Menu.VerticalOffset = -1;
+                Menu.Open();
+                Menu.Closed += Menu_Closed;
+            }
+
+            RoutedEventArgs rre = new RoutedEventArgs(MenuClickEvent);
+            RaiseEvent(rre);
+        }
+
+        private void btnMain_RightClick(object? sender, RoutedEventArgs e)
+        {
+            PerformRightClick();
+        }
+
+        private void btnMenu_RightClick(object? sender, RoutedEventArgs e)
+        {
+            PerformRightClick();
+        }
+
+        #region Menu Button Layout
+
+        /// <summary>
+        /// Get or set where the menu button should be placed in relation to the main button. Default is <c>Right</c>.
+        /// </summary>
+        /// <remarks>
+        /// If you want to set this value to <c>Top</c> or <c>Bottom</c>, also consider increasing the height of the button or decreasing the value of <see cref="MenuButtonSize"/>.
+        /// If you set this value to <c>Hidden</c>, the menu button is entirely hidden (and no longer accessible via the mouse).
+        /// </remarks>
+        public PlacementDirection MenuButtonPlacement { get => GetValue(MenuButtonPlacementProperty); set => SetValue(MenuButtonPlacementProperty, value); }
+
+        /// <summary>The backing styled property for <see cref="MenuButtonPlacement"/>. See the related property for details.</summary>
+        public static readonly StyledProperty<PlacementDirection> MenuButtonPlacementProperty
+            = AvaloniaProperty.Register<SplitButton, PlacementDirection>(nameof(MenuButtonPlacement), PlacementDirection.Right);
+
+        /// <summary>
+        /// Get or set the width or height of the menu button. Larger values make the button wider or taller, and takes up a larger portion of the overall control. Default is <c>20</c>.
+        /// </summary>
+        /// <remarks>
+        /// When <see cref="MenuButtonPlacement"/> is set to <c>Left</c> or <c>Right</c>, this affects the width of the menu button.
+        /// When it is set to <c>Top</c> or <c>Bottom</c>, this affects the height of the menu button. When it is set to <c>Hidden</c>, this has no effect.
+        /// </remarks>
+        public double MenuButtonSize { get => GetValue(MenuButtonSizeProperty); set => SetValue(MenuButtonSizeProperty, value); }
+
+        /// <summary>The backing styled property for <see cref="MenuButtonSize"/>. See the related property for details.</summary>
+        public static readonly StyledProperty<double> MenuButtonSizeProperty
+            = AvaloniaProperty.Register<SplitButton, double>(nameof(MenuButtonSize), 20.0);
+
+        #endregion
+        
+        #endregion
+
+        #region Click / Selection Handling
+
+        #region Routed Events
+
+        /// <summary>
+        /// The backing value for the <see cref="Click"/> event. See the related event for more details.
+        /// </summary>
+        public static readonly RoutedEvent ClickEvent = RoutedEvent.Register<RoutedEventArgs>(
+            "Click", RoutingStrategies.Bubble, typeof(SplitButton));
+
+        /// <summary>
+        /// Raised when the user clicks on the main button (not the menu button), via a mouse click or via the keyboard.
+        /// </summary>
+        public event EventHandler<RoutedEventArgs> Click
+        {
+            add { AddHandler(ClickEvent, value); }
+            remove { RemoveHandler(ClickEvent, value); }
+        }
+
+        /// <summary>
+        /// The backing value for the <see cref="MenuClick"/> event. See the related event for more details.
+        /// </summary>
+        public static readonly RoutedEvent MenuClickEvent = RoutedEvent.Register<RoutedEventArgs>(
+            "MenuClick", RoutingStrategies.Bubble, typeof(SplitButton));
+
+        /// <summary>
+        /// Raised when the user clicks on the menu button (not the main button), via a mouse click or via the keyboard.
+        /// </summary>
+        public event EventHandler<RoutedEventArgs> MenuClick
+        {
+            add { AddHandler(MenuClickEvent, value); }
+            remove { RemoveHandler(MenuClickEvent, value); }
+        }
+
+        /// <summary>
+        /// The backing value for the <see cref="RightClick"/> event. See the related event for more details.
+        /// </summary>
+        public static readonly RoutedEvent RightClickEvent = RoutedEvent.Register<RoutedEventArgs>(
+            "RightClick", RoutingStrategies.Bubble, typeof(SplitButton));
+
+        /// <summary>
+        /// Raised when the user right-clicks on the button, via a mouse click or via the keyboard.
+        /// </summary>
+        public event EventHandler<RoutedEventArgs> RightClick
+        {
+            add { AddHandler(RightClickEvent, value); }
+            remove { RemoveHandler(RightClickEvent, value); }
+        }
+
+        /// <summary>
+        /// Defines the <see cref="IsSelectedChanged"/> event.
+        /// </summary>
+        public static readonly RoutedEvent<ItemSelectionChangedEventArgs> IsSelectedChangedEvent =
+            RoutedEvent.Register<SplitButton, ItemSelectionChangedEventArgs>(nameof(IsSelectedChanged), RoutingStrategies.Bubble);
+
+        /// <summary>
+        /// Raised when the user clicks the button with the right mouse.
+        /// </summary>
+        public event EventHandler<ItemSelectionChangedEventArgs> IsSelectedChanged
+        {
+            add { AddHandler(IsSelectedChangedEvent, value); }
+            remove { RemoveHandler(IsSelectedChangedEvent, value); }
+        }
+
+        /// <summary>
+        /// Defines the <see cref="CanSelectChanged"/> event.
+        /// </summary>
+        public static readonly RoutedEvent<RoutedEventArgs> CanSelectChangedEvent =
+            RoutedEvent.Register<SplitButton, RoutedEventArgs>(nameof(CanSelectChanged), RoutingStrategies.Bubble);
+
+        /// <summary>
+        /// Raised when the user right-clicks on the button, via a mouse click or via the keyboard.
+        /// </summary>
+        public event EventHandler<RoutedEventArgs> CanSelectChanged
+        {
+            add { AddHandler(CanSelectChangedEvent, value); }
+            remove { RemoveHandler(CanSelectChangedEvent, value); }
+        }
+
+        #endregion
+
+        #region Selection Properties
+
+        bool sel = false;
+
+        /// <summary>
+        /// Gets or sets whether this button is selected. This property (combined with <c>SelectOnClick</c>) allows the button to function like a ToggleButton.
+        /// </summary>
+        /// <remarks>
+        /// A selected button will have a slightly different visual appearance to differentiate it as being selected. This will include, by default, the border being a bit thicker.
+        /// This can be changed via the <see cref="BorderSelectionThickness"/> property. You can also directly edit the brushes used via the <see cref="SelectedBrush"/> and
+        /// <see cref="BorderSelectedBrush"/> properties.
+        /// </remarks>
+        public bool IsSelected
+        {
+            get
+            {
+                return sel;
+            }
+            set
+            {
+                SetIsSelectedWithSource(value, SelectionChangeTrigger.CodeUnknown);
+            }
+        }
+
+        #region SetIsSelectedWithSource
+        /// <summary>
+        /// Set the <see cref="IsSelected"/> value of this control, while also defining how the selection was changed.
+        /// </summary>
+        /// <param name="value">The value to set <see cref="IsSelected"/> to.</param>
+        /// <param name="triggerMethod">The source or method used to trigger the change in selection.</param>
+        /// <param name="triggerSource">The object that triggered the change.</param>
+        /// <remarks>If <see cref="CanSelect"/> is set to <c>false</c>, then nothing will occur (silent fail).</remarks>
+        public void SetIsSelectedWithSource(bool value, SelectionChangeTrigger triggerMethod, object? triggerSource = null)
+        {
+            if (CanSelect)
+            {
+                bool curVal = sel;
+                SetAndRaise(IsSelectedProperty, ref sel, value);
+                //PseudoClasses.Set(pcSel, value);
+
+                if (curVal != sel)
+                {
+                    ItemSelectionChangedEventArgs e = new ItemSelectionChangedEventArgs(IsSelectedChangedEvent, curVal, sel, triggerMethod, triggerSource);
+                    RaiseEvent(e);
+                }
+            }
+        }
+        #endregion
+
+        /// <summary>The backing direct property for <see cref="IsSelected"/>. See the related property for details.</summary>
+        public static readonly DirectProperty<SplitButton, bool> IsSelectedProperty
+            = FlatButton.IsSelectedProperty.AddOwner<SplitButton>((s) => s.IsSelected, (s, v) => s.IsSelected = v, unsetValue: false);
+
+
+        /// <summary>
+        /// Gets or sets whether the button should change its IsSelected property when a click is performed. With this enabled, this allows the button to take on the functionality of a ToggleButton.
+        /// </summary>
+        /// <remarks>
+        /// While SelectOnClick is true, the button will toggle between <see cref="IsSelected"/> being true and false (similar to a ToggleButton). A selected button will, by default, have some visual
+        /// differences to help make it look distinct from unselected buttons. The button's Click event will still be raised while this property is set to <c>true</c>, but the event occurs after the
+        /// IsSelected property has already changed. While you could use the Click event to check when the button's IsSelected property is changed, it is better to use the IsSelectedChanged event,
+        /// in case of situations where IsSelected is changed via methods other than clicking, such as programmatically or via WPF binding.
+        /// </remarks>
+        public bool SelectOnClick { get => GetValue(SelectOnClickProperty); set => SetValue(SelectOnClickProperty, value); }
+
+        /// <summary>The backing styled property for <see cref="SelectOnClick"/>. See the related property for details.</summary>
+        public static readonly StyledProperty<bool> SelectOnClickProperty
+            = FlatButton.SelectOnClickProperty.AddOwner<SplitButton>();
+
+        /// <summary>
+        /// Get or set if this control can be selected.
+        /// </summary>
+        /// <remarks>
+        /// If this is set to <c>false</c>, then this control cannot be selected via any method - even programmatically. Setting this to <c>false</c> will also deselect this control, 
+        /// if currently selected. For more fine-tuned control, you can use <see cref="SelectOnClick"/> to limit how the user can select this control, 
+        /// while still being able to change the selection status via <see cref="IsSelected"/>.
+        /// </remarks>
+        public bool CanSelect { get => GetValue(CanSelectProperty); set => SetValue(CanSelectProperty, value); }
+
+        /// <summary>The backing styled property for <see cref="CanSelect"/>. See the related property for details.</summary>
+        public static readonly StyledProperty<bool> CanSelectProperty
+            = FlatButton.CanSelectProperty.AddOwner<SplitButton>();
+
+
+        private void OnCanSelectChanged(AvaloniaPropertyChangedEventArgs e)
+        {
+            if (((bool?)e.NewValue ?? true) == false)
+            {
+                if (IsSelected)
+                {
+                    sel = false;
+
+                    ItemSelectionChangedEventArgs re = new ItemSelectionChangedEventArgs(IsSelectedChangedEvent, true, false, SelectionChangeTrigger.DisableSelecting, e.Sender);
+                    RaiseEvent(re);
+                }
+            }
+
+            RoutedEventArgs cre = new RoutedEventArgs(CanSelectChangedEvent, this);
+            RaiseEvent(cre);
+        }
+
+        #endregion
+
+
+        // If the button is prepared by PerformPress, perform the Click actions, including raising the Click event.
+        void PerformRightClick()
+        {
+            RoutedEventArgs rre = new RoutedEventArgs(RightClickEvent);
+            RaiseEvent(rre);
+        }
+
+        /// <summary>
+        /// Perform a click on the main button programmatically. The button responds the same way as if it was clicked by the user.
+        /// </summary>
+        public void DoClick()
+        {
+            OnClick();
+        }
+
+        /// <summary>
+        /// Defines the actions the button performs when it is clicked.
+        /// </summary>
+        protected void OnClick()
+        {
+            if (SelectOnClick)
+            {
+                SetIsSelectedWithSource(!IsSelected, SelectionChangeTrigger.ControlClick, this);
+            }
+
+            RoutedEventArgs rre = new RoutedEventArgs(ClickEvent);
+            RaiseEvent(rre);
+        }
+
+        #endregion
+
         #region Template IO
 
         /// <inheritdoc/>
@@ -361,6 +693,15 @@ namespace SolidShineUi
             base.OnApplyTemplate(e);
 
             LoadTemplateItems(e);
+
+            if (itemsLoaded)
+            {
+                btnMain!.Click += btnMain_Click;
+                btnMenu!.Click += btnMenu_Click;
+
+                btnMain.RightClick += btnMain_RightClick;
+                btnMenu.RightClick += btnMenu_RightClick;
+            }
         }
 
         bool itemsLoaded = false;
@@ -383,5 +724,6 @@ namespace SolidShineUi
         }
 
         #endregion
+
     }
 }
