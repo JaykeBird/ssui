cbc4965e0921222fc6c46d13e65689c3f3217584
JaykeBird <jacobrh94@hotmail.com>
2024-04-13 18:40:15 -0500

    Added properties to SeparatorItem in SsuiSample
    Why not lol. Maybe I'll decide to actually move it into the main SSUI project for others to more easily use.

--- SsuiSample/Resources/SeparatorItem.xaml.cs
+++ SsuiSample/Resources/SeparatorItem.xaml.cs
@@ -1,9 +1,12 @@
 ﻿using SolidShineUi;
+using System.Windows.Media;
+using System.Windows;
+using System.Drawing.Printing;
 
 namespace SsuiSample.Resources
 {
     /// <summary>
-    /// Interaction logic for SeparatorItem.xaml
+    /// A basic separator
     /// </summary>
     public partial class SeparatorItem : SelectableUserControl
     {
@@ -11,5 +14,35 @@ namespace SsuiSample.Resources
         {
             InitializeComponent();
         }
+
+        public override void ApplyColorScheme(ColorScheme cs)
+        {
+            base.ApplyColorScheme(cs);
+
+            SeparatorBrush = cs.BorderColor.ToBrush();
+        }
+
+        public Brush SeparatorBrush { get => (Brush)GetValue(SeparatorBrushProperty); set => SetValue(SeparatorBrushProperty, value); }
+
+        /// <summary>The backing dependency property for <see cref="SeparatorBrush"/>. See the related property for details.</summary>
+        public static DependencyProperty SeparatorBrushProperty
+            = DependencyProperty.Register(nameof(SeparatorBrush), typeof(Brush), typeof(SeparatorItem),
+            new FrameworkPropertyMetadata(Colors.Black.ToBrush()));
+
+        public Thickness SeparatorMargin { get => (Thickness)GetValue(SeparatorMarginProperty); set => SetValue(SeparatorMarginProperty, value); }
+
+        /// <summary>The backing dependency property for <see cref="SeparatorMargin"/>. See the related property for details.</summary>
+        public static DependencyProperty SeparatorMarginProperty
+            = DependencyProperty.Register(nameof(SeparatorMargin), typeof(Thickness), typeof(SeparatorItem),
+            new FrameworkPropertyMetadata(new Thickness(5, 0, 5, 0)));
+
+        public double SeparatorHeight { get => (double)GetValue(SeparatorHeightProperty); set => SetValue(SeparatorHeightProperty, value); }
+
+        /// <summary>The backing dependency property for <see cref="SeparatorHeight"/>. See the related property for details.</summary>
+        public static DependencyProperty SeparatorHeightProperty
+            = DependencyProperty.Register(nameof(SeparatorHeight), typeof(double), typeof(SeparatorItem),
+            new FrameworkPropertyMetadata(1.0));
+
+
     }
 }
