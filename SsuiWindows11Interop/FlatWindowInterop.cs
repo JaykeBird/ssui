1beddf53c1a63ea8a914cb613f9208b988f06f43
JaykeBird MPC <jacobrh94@hotmail.com>
2025-01-23 23:02:55 -0600

    Updates/tests with SsuiWindows11Interop
    Tried to test the SsuiWindows11Interop code, but I ran into the same bug I was dealing with earlier (https://github.com/microsoft/WindowsAppSDK/issues/3410), sooo I wasn't able to actually successfully test it. I was able to fix a few things that clearly were problems.
    
    Unfortunately, since the Windows App SDK provides the connecting bits of code I need in order to enable the features that I'm looking for... this is where I get stuck.

--- SsuiWindows11Interop/FlatWindowInterop.cs
+++ SsuiWindows11Interop/FlatWindowInterop.cs
@@ -1,12 +1,28 @@
 ﻿using Microsoft.UI.Windowing;
 using System.Windows.Interop;
+//using Windows.UI.WindowManagement;
 
 namespace SsuiWindows11Interop
 {
     public static class FlatWindowInterop
     {
+        // while in theory this code should work, my issue is that I'm not able to properly test it
+        // as I encounter a bug when trying to run a WPF app that references the Windows App SDK:
+        // https://github.com/microsoft/WindowsAppSDK/issues/3410
 
-        public static void RenderWindowsCaptionButtons(ref FlatWindow w)
+        /// <summary>
+        /// Use Windows 11's own caption buttons for the corner of the window, rather than Solid Shine UI's caption buttons.
+        /// </summary>
+        /// <param name="w">The window to update.</param>
+        /// <remarks>
+        /// This tells Windows to render the Windows 11 caption buttons in the corner (the minimize/maximize/close buttons),
+        /// which can then benefit from built-in Windows 11 features like the Snap regions under the Maximize button, as well
+        /// as just fitting in better with how a standard window looks in Windows 11.
+        /// This then tells the FlatWindow itself to not render its own caption buttons, since we're using the Windows 11 ones.
+        /// <para/>
+        /// This requires getting the window's handle, so it's best to use this in the window's SourceInitialized event handler.
+        /// </remarks>
+        public static void RenderWindowsCaptionButtons(this FlatWindow w)
         {
             if (!AppWindowTitleBar.IsCustomizationSupported()) return;
 
@@ -18,7 +34,7 @@ namespace SsuiWindows11Interop
             AppWindow appWindow = AppWindow.GetFromWindowId(wid);
 
             appWindow.TitleBar.ExtendsContentIntoTitleBar = true;
-            w.CaptionDisplayType = ChromeButtons.CaptionType.None;
+            w.CaptionDisplayType = CaptionType.None;
         }
 
     }
