38b408d9ce29298658d42b4a5ca93cac699211fc
JaykeBird MPC <jacobrh94@hotmail.com>
2025-03-16 20:59:34 -0500

    Backport CursorEditor from 2.0
    There's nothing in here that isn't relying upon my other SSUI 2.0 work, so I figure I may as well backport this so that it can be used here.
    
    This allows the Cursor property in WPF controls to be updated and changed. It also supports reading cursors from a file. As part of this, I had to create an extension method that allowed me to pull in the corresponding Cursor type from the CursorType enum, so that was also backported to here.

---
+++ SolidShineUi/PropertyList/PropertyEditors/CursorEditor.xaml.cs
@@ -0,0 +1,241 @@
+﻿using SolidShineUi.Utils;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace SolidShineUi.PropertyList.PropertyEditors
+{
+    /// <summary>
+    /// A property editor for <see cref="Cursor"/> objects.
+    /// </summary>
+    public partial class CursorEditor : UserControl, IPropertyEditor
+    {
+        /// <summary>
+        /// Create a CursorEditor.
+        /// </summary>
+        public CursorEditor()
+        {
+            InitializeComponent();
+        }
+
+        /// <inheritdoc/>
+        public List<Type> ValidTypes => new List<Type> { typeof(Cursor) };
+
+        /// <inheritdoc/>
+        public bool EditorAllowsModifying => true;
+
+        /// <inheritdoc/>
+        public bool IsPropertyWritable
+        {
+            get => cbbCursors.IsEnabled;
+            set
+            {
+                cbbCursors.IsEnabled = value;
+                fs.IsEnabled = value;
+                btnMenu.IsEnabled = value;
+            }
+        }
+
+        /// <inheritdoc/>
+        public ColorScheme ColorScheme { get => _cs; set => ApplyColorScheme(value); }
+
+        private ColorScheme _cs = new ColorScheme();
+
+        /// <inheritdoc/>
+        public void ApplyColorScheme(ColorScheme colorScheme)
+        {
+            _cs = colorScheme;
+
+            fs.ColorScheme = _cs;
+            btnMenu.ColorScheme = _cs;
+            imgItalic.Source = IconLoader.LoadIcon("Select", _cs);
+            imgMenu.Source = IconLoader.LoadIcon("ThreeDots", _cs);
+        }
+
+        /// <inheritdoc/>
+        public ExperimentalPropertyList ParentPropertyList { set { } }
+
+        /// <inheritdoc/>
+#if NETCOREAPP
+        public event EventHandler? ValueChanged;
+#else
+        public event EventHandler ValueChanged;
+#endif
+
+        /// <inheritdoc/>
+        public FrameworkElement GetFrameworkElement()
+        {
+            return this;
+        }
+
+#if NETCOREAPP
+        Cursor? cval = Cursors.Arrow;
+#else
+        Cursor cval = Cursors.Arrow;
+#endif
+
+        bool _internalAction = false;
+
+        /// <inheritdoc/>
+#if NETCOREAPP
+        public object? GetValue()
+#else
+        public object GetValue()
+#endif
+        {
+            return cval;
+        }
+
+        /// <inheritdoc/>
+#if NETCOREAPP
+        public void LoadValue(object? value, Type type)
+#else
+        public void LoadValue(object value, Type type)
+#endif
+        {
+            if (value == null)
+            {
+                _internalAction = true;
+                cbbCursors.SelectedEnumValue = CursorType.Arrow;
+                SetValueToNull();
+                _internalAction = false;
+            }
+            else if (value is Cursor c)
+            {
+                string curVal = c.ToString();
+                _internalAction = true;
+
+                if (Enum.TryParse(curVal, true, out CursorType ctt))
+                {
+                    DisplayDropDown();
+
+                    // this is a predefined cursor
+                    cbbCursors.SelectedEnumValue = ctt;
+                    cval = ctt.ToCursor();
+                }
+                else
+                {
+                    DisplayFileSelect();
+
+                    if (File.Exists(curVal))
+                    {
+                        // this is a cursor from a file
+                        fs.SelectedFiles.Clear();
+                        fs.SelectedFiles.Add(curVal);
+                    }
+                    else
+                    {
+                        // this probably was a cursor from a file, but it doesn't exist
+                        fs.SelectedFiles.Clear();
+                        cval = Cursors.Arrow;
+                    }
+                }
+                _internalAction = false;
+            }
+        }
+
+#if NETCOREAPP
+        void SetValue(Cursor? c)
+#else
+        void SetValue(Cursor c)
+#endif
+        {
+            cval = c;
+            ValueChanged?.Invoke(this, EventArgs.Empty);
+        }
+
+        private void cbbCursors_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (!_internalAction)
+            {
+                SetValue(cbbCursors.SelectedEnumValueAsEnum<CursorType>().ToCursor());
+            }
+        }
+
+        private void fs_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+            if (!_internalAction)
+            {
+                if (fs.SelectedFiles.Count > 0)
+                {
+                    SetValue(new Cursor(fs.SelectedFiles[0]));
+                }
+                else
+                {
+                    SetValue(Cursors.Arrow);
+                }
+            }
+        }
+
+        void DisplayFileSelect()
+        {
+            cbbCursors.Visibility = Visibility.Collapsed;
+            stkFile.Visibility = Visibility.Visible;
+            txtNull.Visibility = Visibility.Collapsed;
+
+            mnuDropdown.IsChecked = false;
+            mnuFileSelect.IsChecked = true;
+            mnuNull.IsChecked = false;
+
+            if (fs.SelectedFiles.Count > 0)
+            {
+#if NET40 || NET45
+                SetValue(new Cursor(fs.SelectedFiles[0]));
+#else
+                SetValue(new Cursor(fs.SelectedFiles[0], false));
+#endif
+            }
+            else
+            {
+                SetValue(Cursors.Arrow);
+            }
+        }
+
+        void DisplayDropDown()
+        {
+            cbbCursors.Visibility = Visibility.Visible;
+            stkFile.Visibility = Visibility.Collapsed;
+            txtNull.Visibility = Visibility.Collapsed;
+
+            mnuDropdown.IsChecked = true;
+            mnuFileSelect.IsChecked = false;
+            mnuNull.IsChecked = false;
+
+            SetValue(cbbCursors.SelectedEnumValueAsEnum<CursorType>().ToCursor());
+        }
+
+        void SetValueToNull()
+        {
+            cbbCursors.Visibility = Visibility.Collapsed;
+            stkFile.Visibility = Visibility.Collapsed;
+            txtNull.Visibility = Visibility.Visible;
+
+            mnuDropdown.IsChecked = false;
+            mnuFileSelect.IsChecked = false;
+            mnuNull.IsChecked = true;
+
+            if (!_internalAction)
+            {
+                SetValue(null);
+            }
+        }
+
+        private void mnuDropdown_Click(object sender, RoutedEventArgs e)
+        {
+            DisplayDropDown();
+        }
+
+        private void mnuFileSelect_Click(object sender, RoutedEventArgs e)
+        {
+            DisplayFileSelect();
+        }
+
+        private void mnuNull_Click(object sender, RoutedEventArgs e)
+        {
+            SetValueToNull();
+        }
+    }
+}
---
+++ SolidShineUi/Utils/CursorTypeExtension.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace SolidShineUi.Utils
+{
+    /// <summary>
+    /// Adds an extension method to convert <see cref="CursorType"/> to a <see cref="Cursor"/>.
+    /// </summary>
+    public static class CursorTypeExtension
+    {
+        /// <summary>
+        /// Return a <see cref="Cursor"/> that is represented by the this <paramref name="cursorType"/>.
+        /// </summary>
+        /// <param name="cursorType">The cursor type value to get a cursor from.</param>
+        /// <returns>A cursor with this value.</returns>
+        /// <remarks>
+        /// WPF internally has a function to load a Cursor from a CursorType, but that function/constructor isn't publicly exposed.
+        /// This method uses the <see cref="Cursors"/> helper class and a <c>switch</c> block to achieve the same goal in a more roundabout way.
+        /// </remarks>
+        public static Cursor ToCursor(this CursorType cursorType)
+        {
+            switch (cursorType)
+            {
+                case CursorType.None:
+                    return Cursors.None;
+                case CursorType.No:
+                    return Cursors.No;
+                case CursorType.Arrow:
+                    return Cursors.Arrow;
+                case CursorType.AppStarting:
+                    return Cursors.AppStarting;
+                case CursorType.Cross:
+                    return Cursors.Cross;
+                case CursorType.Help:
+                    return Cursors.Help;
+                case CursorType.IBeam:
+                    return Cursors.IBeam;
+                case CursorType.SizeAll:
+                    return Cursors.SizeAll;
+                case CursorType.SizeNESW:
+                    return Cursors.SizeNESW;
+                case CursorType.SizeNS:
+                    return Cursors.SizeNS;
+                case CursorType.SizeNWSE:
+                    return Cursors.SizeNWSE;
+                case CursorType.SizeWE:
+                    return Cursors.SizeWE;
+                case CursorType.UpArrow:
+                    return Cursors.UpArrow;
+                case CursorType.Wait:
+                    return Cursors.Wait;
+                case CursorType.Hand:
+                    return Cursors.Hand;
+                case CursorType.Pen:
+                    return Cursors.Pen;
+                case CursorType.ScrollNS:
+                    return Cursors.ScrollNS;
+                case CursorType.ScrollWE:
+                    return Cursors.ScrollWE;
+                case CursorType.ScrollAll:
+                    return Cursors.ScrollAll;
+                case CursorType.ScrollN:
+                    return Cursors.ScrollN;
+                case CursorType.ScrollS:
+                    return Cursors.ScrollS;
+                case CursorType.ScrollW:
+                    return Cursors.ScrollW;
+                case CursorType.ScrollE:
+                    return Cursors.ScrollE;
+                case CursorType.ScrollNW:
+                    return Cursors.ScrollNW;
+                case CursorType.ScrollNE:
+                    return Cursors.ScrollNE;
+                case CursorType.ScrollSW:
+                    return Cursors.ScrollSW;
+                case CursorType.ScrollSE:
+                    return Cursors.ScrollSE;
+                case CursorType.ArrowCD:
+                    return Cursors.ArrowCD;
+                default:
+                    // just return the base cursor
+                    return Cursors.Arrow;
+            }
+        }
+
+    }
+}
