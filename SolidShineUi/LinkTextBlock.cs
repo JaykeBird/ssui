2c2622d497c5ff5db1ca8f35b188955ed6305f06
JaykeBird MPC <jacobrh94@hotmail.com>
2025-09-27 11:52:24 -0500

    Rearrange/improve LinkTextBlock code
    I didn't realize that I haven't touched or improved this code at all. I know that I made some modifications to this in the Avalonia branch, but I'm not sure how far or what kind of changes I made.
    
    Anyway, I rearranged the code and put it into regions. I also updated the XML comments for the dependency properties and Click routed event. I removed the override for the IsEnabled property, and instead just put in a listener for the underlying FrameworkElement's IsEnabled dependency property.
    
    Finally, I changed the un-highlight code to not remove text decorations if UnderLineOnHighlight is set to false.
    
    I'm kind of on the fence about making a 1.9.10, but if I do make it, this would be in it.

--- SolidShineUi/LinkTextBlock.cs
+++ SolidShineUi/LinkTextBlock.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SolidShineUi.Utils;
+using System;
 using System.ComponentModel;
 using System.Diagnostics;
 using System.Windows;
@@ -9,141 +10,19 @@ using System.Windows.Media;
 namespace SolidShineUi
 {
     /// <summary>
-    /// A text block that has a Click event, thus looking and acting more like a generic hyperlink from a web page.
+    /// A text block that has a Click event, and looking and acting more like a basic hyperlink from a web page.
     /// </summary>
     [DefaultEvent("Click")]
     public class LinkTextBlock : TextBlock
     {
-        bool isHighlighted = false;
 
-#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
-        public static readonly RoutedEvent ClickEvent = EventManager.RegisterRoutedEvent(
-            "Click", RoutingStrategy.Bubble, typeof(RoutedEventHandler), typeof(LinkTextBlock));
-#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
+        #region Constructors
 
-        /// <summary>
-        /// This event is raised when the control is clicked.
-        /// </summary>
-        public event RoutedEventHandler Click
+        static LinkTextBlock()
         {
-            add { AddHandler(ClickEvent, value); }
-            remove { RemoveHandler(ClickEvent, value); }
-        }
-
-#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
-        public static readonly DependencyProperty TextBrushProperty = DependencyProperty.Register(
-            "TextBrush", typeof(Brush), typeof(LinkTextBlock),
-            new PropertyMetadata(Color.FromRgb(0, 102, 204).ToBrush(), new PropertyChangedCallback(OnInternalBrushChanged)));
-
-        public static readonly DependencyProperty HighlightBrushProperty = DependencyProperty.Register(
-            "HighlightBrush", typeof(Brush), typeof(LinkTextBlock),
-            new PropertyMetadata(Color.FromRgb(51, 153, 255).ToBrush(), new PropertyChangedCallback(OnInternalBrushChanged)));
-
-        public static readonly DependencyProperty DisabledBrushProperty = DependencyProperty.Register(
-            "DisabledBrush", typeof(Brush), typeof(LinkTextBlock),
-            new PropertyMetadata(Color.FromRgb(120, 120, 120).ToBrush(), new PropertyChangedCallback(OnInternalBrushChanged)));
-
-        public static readonly DependencyProperty AutoSetLinkFromTextProperty = DependencyProperty.Register(
-            "AutoSetLinkFromText", typeof(bool), typeof(LinkTextBlock),
-            new PropertyMetadata(false));
-
-        public static readonly DependencyProperty UnderlineOnHighlightProperty = DependencyProperty.Register(
-            "UnderlineOnHighlight", typeof(bool), typeof(LinkTextBlock),
-            new PropertyMetadata(true));
-#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
-
-        #region Brush change hook
-        private static void OnInternalBrushChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
-        {
-            if (d is LinkTextBlock t)
-            {
-                t.InternalBrushChanged?.Invoke(t, e);
-            }
-        }
-
-        /// <summary>
-        /// Internal event for handling a property changed. Please view the event that is not prefixed as "Internal".
-        /// </summary>
-        protected event DependencyPropertyChangedEventHandler InternalBrushChanged;
-
-        private void LinkTextBlock_InternalBrushChanged(object sender, DependencyPropertyChangedEventArgs e)
-        {
-            if (IsEnabled)
-            {
-                if (isHighlighted)
-                {
-                    Foreground = HighlightBrush;
-                }
-                else
-                {
-                    Foreground = TextBrush;
-                }
-            }
-            else
-            {
-                Foreground = DisabledBrush;
-            }
-        }
-
-        #endregion
-
-        /// <summary>
-        /// Get or set the standard foreground brush for the text. This overwrites the Foreground property.
-        /// </summary>
-        [Category("Brushes"), Description("Set brush for text. This overwrites the Foreground property.")]
-        public Brush TextBrush
-        {
-            get => (Brush)GetValue(TextBrushProperty);
-            set => SetValue(TextBrushProperty, value);
-        }
-
-        /// <summary>
-        /// Get or set the brush for when the control has mouseover or keyboard focus.
-        /// </summary>
-        [Category("Brushes"), Description("Set brush for when control has mouseover or keyboard focus.")]
-        public Brush HighlightBrush
-        {
-            get => (Brush)GetValue(HighlightBrushProperty);
-            set => SetValue(HighlightBrushProperty, value);
-        }
-
-        /// <summary>
-        /// Get or set the brush for when the control is disabled.
-        /// </summary>
-        [Category("Brushes"), Description("Set brush for when the control is disabled.")]
-        public Brush DisabledBrush
-        {
-            get => (Brush)GetValue(DisabledBrushProperty);
-            set => SetValue(DisabledBrushProperty, value);
-        }
-
-        /// <summary>
-        /// Get or set if the LinkTextBlock should automatically create a link based upon the Text property. See remarks for more details.
-        /// </summary>
-        /// <remarks>
-        /// When AutoSetLinkFromText is set to <c>true</c>, the LinkTextBlock attempts to open a file or URL based upon the value of the Text property when the user clicks on the control.
-        /// For example, if this property is set to <c>true</c> and the Text property is set to <c>"https://microsoft.com"</c>, the LinkTextBlock will open "https://microsoft.com" when the control is clicked,
-        /// even if a Click event handler is not attached. Note that for web links, the scheme/protocol should be present (i.e. "http", "https", "ftp", etc.).
-        /// <para />
-        /// Note that the Click event will still fire even if this property is set to <c>true</c>.
-        /// <para/>
-        /// If an error occurs while attempting to open the link, nothing happens. If a debugger is attached, the LinkTextBlock attempts to log a message to the debugger.
-        /// </remarks>
-        [Category("Common"), Description("Set whether to open a link when the control is clicked. Link is set by Text property.")]
-        public bool AutoSetLinkFromText
-        {
-            get => (bool)GetValue(AutoSetLinkFromTextProperty);
-            set => SetValue(AutoSetLinkFromTextProperty, value);
-        }
-
-        /// <summary>
-        /// Get or set if the text should be underlined when the control has mouseover or keyboard focus.
-        /// </summary>
-        [Category("Appearance"), Description("Set if the text should be underlined when highlighted.")]
-        public bool UnderlineOnHighlight
-        {
-            get => (bool)GetValue(UnderlineOnHighlightProperty);
-            set => SetValue(UnderlineOnHighlightProperty, value);
+            // add listener for IsEnabled changing
+            IsEnabledProperty.OverrideMetadata(typeof(LinkTextBlock), new FrameworkPropertyMetadata(
+                new PropertyChangedCallback((d, e) => d.PerformAs<LinkTextBlock, bool>(e.NewValue, (l, v) => l.IsEnabledValueChanged(v)))));
         }
 
         /// <summary>
@@ -195,28 +74,146 @@ namespace SolidShineUi
             InternalBrushChanged += LinkTextBlock_InternalBrushChanged;
         }
 
-        /// <summary>
-        /// Gets or sets a value indicating whether this element is enabled in the user interface (UI).
-        /// </summary>
-        new public bool IsEnabled
-        {
-            get { return base.IsEnabled; }
-            set
-            {
-                base.IsEnabled = value;
+        #endregion
 
-                if (value == false)
+        bool isHighlighted = false;
+
+        #region Brushes
+
+        /// <summary>The backing dependency property for <see cref="TextBrush"/>. See the related property for details.</summary>
+        public static readonly DependencyProperty TextBrushProperty = DependencyProperty.Register(
+            nameof(TextBrush), typeof(Brush), typeof(LinkTextBlock),
+            new PropertyMetadata(Color.FromRgb(0, 102, 204).ToBrush(), (d, e) => d.PerformAs<LinkTextBlock>((o) => o.OnInternalBrushChanged(e))));
+
+        /// <summary>The backing dependency property for <see cref="HighlightBrush"/>. See the related property for details.</summary>
+        public static readonly DependencyProperty HighlightBrushProperty = DependencyProperty.Register(
+            nameof(HighlightBrush), typeof(Brush), typeof(LinkTextBlock),
+            new PropertyMetadata(Color.FromRgb(51, 153, 255).ToBrush(), (d, e) => d.PerformAs<LinkTextBlock>((o) => o.OnInternalBrushChanged(e))));
+
+        /// <summary>The backing dependency property for <see cref="DisabledBrush"/>. See the related property for details.</summary>
+        public static readonly DependencyProperty DisabledBrushProperty = DependencyProperty.Register(
+            nameof(DisabledBrush), typeof(Brush), typeof(LinkTextBlock),
+            new PropertyMetadata(Color.FromRgb(120, 120, 120).ToBrush(), (d, e) => d.PerformAs<LinkTextBlock>((o) => o.OnInternalBrushChanged(e))));
+
+
+        /// <summary>
+        /// Get or set the standard foreground brush for the text. This overwrites the Foreground property.
+        /// </summary>
+        [Category("Brushes"), Description("Set brush for text. This overwrites the Foreground property.")]
+        public Brush TextBrush
+        {
+            get => (Brush)GetValue(TextBrushProperty);
+            set => SetValue(TextBrushProperty, value);
+        }
+
+        /// <summary>
+        /// Get or set the brush for when the control has mouseover or keyboard focus.
+        /// </summary>
+        [Category("Brushes"), Description("Set brush for when control has mouseover or keyboard focus.")]
+        public Brush HighlightBrush
+        {
+            get => (Brush)GetValue(HighlightBrushProperty);
+            set => SetValue(HighlightBrushProperty, value);
+        }
+
+        /// <summary>
+        /// Get or set the brush for when the control is disabled.
+        /// </summary>
+        [Category("Brushes"), Description("Set brush for when the control is disabled.")]
+        public Brush DisabledBrush
+        {
+            get => (Brush)GetValue(DisabledBrushProperty);
+            set => SetValue(DisabledBrushProperty, value);
+        }
+
+        private void OnInternalBrushChanged(DependencyPropertyChangedEventArgs e)
+        {
+            if (IsEnabled)
+            {
+                if (isHighlighted)
                 {
-                    Foreground = DisabledBrush;
+                    Foreground = HighlightBrush;
                 }
                 else
                 {
                     Foreground = TextBrush;
                 }
             }
+            else
+            {
+                Foreground = DisabledBrush;
+            }
         }
 
-        private void Highlight()
+        /// <summary>
+        /// Internal event for handling a property changed. Please view the event that is not prefixed as "Internal".
+        /// </summary>
+        protected event DependencyPropertyChangedEventHandler InternalBrushChanged;
+
+        private void LinkTextBlock_InternalBrushChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+
+        }
+
+        #endregion
+
+        #region Other Properties
+
+        /// <summary>The backing dependency property for <see cref="AutoSetLinkFromText"/>. See the related property for details.</summary>
+        public static readonly DependencyProperty AutoSetLinkFromTextProperty = DependencyProperty.Register(
+            nameof(AutoSetLinkFromText), typeof(bool), typeof(LinkTextBlock), new PropertyMetadata(false));
+
+        /// <summary>The backing dependency property for <see cref="UnderlineOnHighlight"/>. See the related property for details.</summary>
+        public static readonly DependencyProperty UnderlineOnHighlightProperty = DependencyProperty.Register(
+            nameof(UnderlineOnHighlight), typeof(bool), typeof(LinkTextBlock), new PropertyMetadata(true));
+
+        /// <summary>
+        /// Get or set if the LinkTextBlock should automatically create a link based upon the Text property. See remarks for more details.
+        /// </summary>
+        /// <remarks>
+        /// When AutoSetLinkFromText is set to <c>true</c>, the LinkTextBlock attempts to open a file or URL based upon the value of the Text property when the user 
+        /// clicks on the control. For example, if this property is set to <c>true</c> and the Text property is set to <c>"https://microsoft.com"</c>, 
+        /// the LinkTextBlock will open "https://microsoft.com" when the control is clicked, even if a Click event handler is not attached. 
+        /// Note that for web links, the scheme/protocol should be present (e.g. "http://", "https://", "ftp://", etc.).
+        /// <para />
+        /// Note that the Click event will still fire even if this property is set to <c>true</c>, after the link is opened.
+        /// <para/>
+        /// If an error occurs while attempting to open the link, nothing happens. If a debugger is attached, the LinkTextBlock attempts to log a message to the debugger.
+        /// </remarks>
+        [Category("Common"), Description("Set whether to open a link when the control is clicked. Link is set by Text property.")]
+        public bool AutoSetLinkFromText
+        {
+            get => (bool)GetValue(AutoSetLinkFromTextProperty);
+            set => SetValue(AutoSetLinkFromTextProperty, value);
+        }
+
+        /// <summary>
+        /// Get or set if the text should be underlined when the control has mouseover or keyboard focus.
+        /// </summary>
+        [Category("Appearance"), Description("Set if the text should be underlined when highlighted.")]
+        public bool UnderlineOnHighlight
+        {
+            get => (bool)GetValue(UnderlineOnHighlightProperty);
+            set => SetValue(UnderlineOnHighlightProperty, value);
+        }
+
+        #endregion
+
+        #region Foreground-modifying methods
+
+        void IsEnabledValueChanged(bool newValue)
+        {
+            if (newValue == false)
+            {
+                Foreground = DisabledBrush;
+            }
+            else
+            {
+                Foreground = TextBrush;
+            }
+        }
+
+        void Highlight()
         {
             if (IsEnabled)
             {
@@ -226,15 +223,19 @@ namespace SolidShineUi
             }
         }
 
-        private void Unlight()
+        void Unlight()
         {
             if (IsEnabled)
             {
-                TextDecorations = null;
+                if (UnderlineOnHighlight) TextDecorations = null;
                 Foreground = TextBrush;
                 isHighlighted = false;
             }
         }
+
+        #endregion
+
+        #region Event Handlers
 
         void LinkTextBlock_MouseEnter(object sender, MouseEventArgs e)
         {
@@ -256,17 +257,6 @@ namespace SolidShineUi
             Unlight();
         }
 
-        private void RaiseClick()
-        {
-            if (AutoSetLinkFromText)
-            {
-                OpenLinkFromText();
-            }
-
-            RoutedEventArgs re = new RoutedEventArgs(ClickEvent);
-            RaiseEvent(re);
-        }
-
 #if NETCOREAPP
         void LinkTextBlock_PreviewTouchUp(object? sender, TouchEventArgs e)
 #else
@@ -286,13 +276,45 @@ namespace SolidShineUi
             RaiseClick();
         }
 
-
         void LinkTextBlock_PreviewKeyUp(object sender, KeyEventArgs e)
         {
             if (e.Key == Key.Enter || e.Key == Key.Space)
             {
                 RaiseClick();
             }
+        }
+
+        #endregion
+
+        #region Click Handling (LinkTextBlock)
+
+        /// <summary>
+        /// The backing routed event object for <see cref="Click"/>. Please see the related event for details.
+        /// </summary>
+        public static readonly RoutedEvent ClickEvent = EventManager.RegisterRoutedEvent(
+            nameof(Click), RoutingStrategy.Bubble, typeof(RoutedEventHandler), typeof(LinkTextBlock));
+
+        /// <summary>
+        /// This event is raised when the control is clicked.
+        /// </summary>
+        public event RoutedEventHandler Click
+        {
+            add { AddHandler(ClickEvent, value); }
+            remove { RemoveHandler(ClickEvent, value); }
+        }
+
+        /// <summary>
+        /// Raise the <see cref="Click"/> event. If <see cref="AutoSetLinkFromText"/> is set to <c>true</c>, this will also trigger opening that link.
+        /// </summary>
+        protected void RaiseClick()
+        {
+            if (AutoSetLinkFromText)
+            {
+                OpenLinkFromText();
+            }
+
+            RoutedEventArgs re = new RoutedEventArgs(ClickEvent);
+            RaiseEvent(re);
         }
 
         void OpenLinkFromText()
@@ -334,47 +356,8 @@ namespace SolidShineUi
                     Debugger.Log(0, "LinkTextBlock", "Link \"" + Text + "\" could not be opened, from LinkTextBlock " + Name + ".\n");
                 }
             }
-            //catch (PlatformNotSupportedException)
-            //{
-            //    try
-            //    {
-            //        // must use UseShellExecute so that it works on .NET Core
-            //        Process.Start(new ProcessStartInfo { FileName = Text });
-            //    }
-            //    catch (ArgumentNullException)
-            //    {
-            //        // could not open the link as it is null
-            //        if (Debugger.IsAttached)
-            //        {
-            //            Debugger.Log(0, "LinkTextBlock", "Link is null, from LinkTextBlock " + Name + ".\n");
-            //        }
-            //    }
-            //    catch (System.IO.FileNotFoundException)
-            //    {
-            //        // could not open the link as it doesn't exist
-            //        if (Debugger.IsAttached)
-            //        {
-            //            Debugger.Log(0, "LinkTextBlock", "Link \"" + Text + "\" does not exist, from LinkTextBlock " + Name + ".\n");
-            //        }
-            //    }
-            //    catch (InvalidOperationException)
-            //    {
-            //        // could not open the link for some reason
-            //        if (Debugger.IsAttached)
-            //        {
-            //            Debugger.Log(0, "LinkTextBlock", "Link \"" + Text + "\" was not defined, from LinkTextBlock " + Name + ".\n");
-            //        }
-            //    }
-            //    catch (Win32Exception)
-            //    {
-            //        // could not open the link for some reason
-            //        if (Debugger.IsAttached)
-            //        {
-            //            Debugger.Log(0, "LinkTextBlock", "Link \"" + Text + "\" could not be opened, from LinkTextBlock " + Name + ".\n");
-            //        }
-            //    }
-            //}
         }
 
+        #endregion
     }
 }
