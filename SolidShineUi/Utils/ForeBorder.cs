58187703fcf2bbf420ee99a751c3563078780da8
JaykeBird <jacobrh94@hotmail.com>
2025-12-15 23:52:06 -0600

    Add ForeBorder control
    This will be useful and helpful later for the issue that I mentioned in one of the recent commits about the highlight foreground property.

---
+++ SolidShineUi/Utils/ForeBorder.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace SolidShineUi.Utils
+{
+    /// <summary>
+    /// A WPF Border with a <c>Foreground</c> property.
+    /// </summary>
+    /// <remarks>
+    /// This UI element uses the Control's <c>Foreground</c> property, so it participates in the inheritance propogation.
+    /// </remarks>
+    public class ForeBorder : Border
+    {
+
+        public DependencyProperty ForegroundProperty = Control.ForegroundProperty.AddOwner(typeof(ForeBorder));
+
+        public Brush Foreground
+        {
+            get => (Brush)GetValue(ForegroundProperty); 
+            set => SetValue(ForegroundProperty, value);
+        }
+
+    }
+}
