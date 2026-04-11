13c5ccb5c1a81873301317cfbc3bdba2f9467357
JaykeBird <jacobrh94@hotmail.com>
2026-04-11 00:40:26 -0500

    Add CsvLineHelper helper class
    This is a pretty decent implementation, and for basic CSV files and situations, this will work well enough. Note that this doesn't read an entire file, and instead just reads a single line. (Perhaps in a future commit, I should add the ability to read a whole file.)
    
    This was originally taken from a StackOverflow answer, but I modified it to make it work better with .NET's nullability, add the ability to change the delimiter character, and a converting overload that accepts a ReadOnlySpan.
    
    As you can probably tell by the other file added in this commit, I have plans for using this in a future commit.

---
+++ SolidShineUi/Converters/DelimitedStringToListConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace SolidShineUi.Converters
+{
+    public class DelimitedStringToListConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value;
+            //throw new NotImplementedException();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value;
+            //throw new NotImplementedException();
+        }
+    }
+}
