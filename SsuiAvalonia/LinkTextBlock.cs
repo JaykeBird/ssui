1b8bcf6e11f7416661a3dd2bf91214ab9028b157
JaykeBird <jacobrh94@hotmail.com>
2026-07-16 14:11:04 -0500

    LinkTextBlock, make separate Avalonia file
    Rather than having the WPF and Avalonia versions of LinkTextBlock in the same file, I've moved out the Avalonia one to its own file.
    
    I liked the idea of this, but I think it made the file a bit too convoluted. And given that all of my other controls have separate files, I'll just do the same here.
    
    Also, this will make it easier when it comes to merge in the 2.0.2 update from the main branch.

---
+++ SsuiAvalonia/LinkTextBlock.cs
@@ -0,0 +1,363 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Media;
+using Avalonia.Layout;
+using Avalonia.Interactivity;
+
+namespace SolidShineUi
+{
+    /// <summary>
+    /// A text block that has a Click event, thus looking and acting more like a generic hyperlink from a web page.
+    /// </summary>
+    [DefaultEvent("Click")]
+    public class LinkTextBlock : TextBlock
+    {
+        bool isHighlighted = false;
+
+        /// <summary>
+        /// The routed event object for the <see cref="Click"/> event. See the related event for more details.
+        /// </summary>
+        public static readonly RoutedEvent<RoutedEventArgs> ClickEvent = RoutedEvent.Register<LinkTextBlock, RoutedEventArgs>
+            (nameof(Click), RoutingStrategies.Bubble);
+
+        /// <summary>
+        /// This event is raised when the control is clicked.
+        /// </summary>
+        public event EventHandler<RoutedEventArgs> Click
+        {
+            add => AddHandler(ClickEvent, value);
+            remove => RemoveHandler(ClickEvent, value);
+        }
+
+        /// <summary>The backing styled property for <see cref="TextBrush"/>. See the related property for details.</summary>
+        public static readonly StyledProperty<Brush> TextBrushProperty = AvaloniaProperty.Register<LinkTextBlock, Brush>(
+            nameof(TextBrush), Color.FromRgb(0, 102, 204).ToBrush());
+
+        /// <summary>The backing styled property for <see cref="HighlightBrush"/>. See the related property for details.</summary>
+        public static readonly StyledProperty<Brush> HighlightBrushProperty = AvaloniaProperty.Register<LinkTextBlock, Brush>(
+            nameof(HighlightBrush), Color.FromRgb(51, 153, 255).ToBrush());
+
+        /// <summary>The backing styled property for <see cref="DisabledBrush"/>. See the related property for details.</summary>
+        public static readonly StyledProperty<Brush> DisabledBrushProperty = AvaloniaProperty.Register<LinkTextBlock, Brush>(
+            nameof(DisabledBrush), Color.FromRgb(120, 120, 120).ToBrush());
+
+        /// <summary>The backing styled property for <see cref="AutoSetLinkFromText"/>. See the related property for details.</summary>
+        public static readonly StyledProperty<bool> AutoSetLinkFromTextProperty = AvaloniaProperty.Register<LinkTextBlock, bool>(
+            nameof(AutoSetLinkFromText), false);
+
+        /// <summary>The backing styled property for <see cref="UnderlineOnHighlight"/>. See the related property for details.</summary>
+        public static readonly StyledProperty<bool> UnderlineOnHighlightProperty = AvaloniaProperty.Register<LinkTextBlock, bool>(
+            nameof(UnderlineOnHighlight), true);
+
+        /// <inheritdoc/>
+        protected override void OnPropertyChanged(AvaloniaPropertyChangedEventArgs change)
+        {
+            base.OnPropertyChanged(change);
+
+            switch (change.Property.Name)
+            {
+                case nameof(TextBrush):
+                case nameof(HighlightBrush):
+                case nameof(DisabledBrush):
+                    HandleInternalBrushChanged();
+                    break;
+            }
+        }
+        
+        private void HandleInternalBrushChanged()
+        {
+            if (IsEnabled)
+            {
+                if (isHighlighted)
+                {
+                    Foreground = HighlightBrush;
+                }
+                else
+                {
+                    Foreground = TextBrush;
+                }
+            }
+            else
+            {
+                Foreground = DisabledBrush;
+            }
+        }
+
+
+        /// <summary>
+        /// Get or set the standard foreground brush for the text. This overwrites the Foreground property.
+        /// </summary>
+        [Category("Brushes"), Description("Set brush for text. This overwrites the Foreground property.")]
+        public Brush TextBrush
+        {
+            get => (Brush)GetValue(TextBrushProperty);
+            set => SetValue(TextBrushProperty, value);
+        }
+
+        /// <summary>
+        /// Get or set the brush for when the control has mouseover or keyboard focus.
+        /// </summary>
+        [Category("Brushes"), Description("Set brush for when control has mouseover or keyboard focus.")]
+        public Brush HighlightBrush
+        {
+            get => (Brush)GetValue(HighlightBrushProperty);
+            set => SetValue(HighlightBrushProperty, value);
+        }
+
+        /// <summary>
+        /// Get or set the brush for when the control is disabled.
+        /// </summary>
+        [Category("Brushes"), Description("Set brush for when the control is disabled.")]
+        public Brush DisabledBrush
+        {
+            get => (Brush)GetValue(DisabledBrushProperty);
+            set => SetValue(DisabledBrushProperty, value);
+        }
+
+        /// <summary>
+        /// Get or set if the LinkTextBlock should automatically create a link based upon the Text property. See remarks for more details.
+        /// </summary>
+        /// <remarks>
+        /// When AutoSetLinkFromText is set to <c>true</c>, the LinkTextBlock attempts to open a file or URL based upon the value of the Text property when the user clicks on the control.
+        /// For example, if this property is set to <c>true</c> and the Text property is set to <c>"https://microsoft.com"</c>, the LinkTextBlock will open "https://microsoft.com" when the control is clicked,
+        /// even if a Click event handler is not attached. Note that for web links, the scheme/protocol should be present (i.e. "http", "https", "ftp", etc.).
+        /// <para />
+        /// Note that the Click event will still fire even if this property is set to <c>true</c>.
+        /// <para/>
+        /// If an error occurs while attempting to open the link, nothing happens. If a debugger is attached, the LinkTextBlock attempts to log a message to the debugger.
+        /// </remarks>
+        [Category("Common"), Description("Set whether to open a link when the control is clicked. Link is set by Text property.")]
+        public bool AutoSetLinkFromText
+        {
+            get => (bool)GetValue(AutoSetLinkFromTextProperty);
+            set => SetValue(AutoSetLinkFromTextProperty, value);
+        }
+
+        /// <summary>
+        /// Get or set if the text should be underlined when the control has mouseover or keyboard focus.
+        /// </summary>
+        [Category("Appearance"), Description("Set if the text should be underlined when highlighted.")]
+        public bool UnderlineOnHighlight
+        {
+            get => (bool)GetValue(UnderlineOnHighlightProperty);
+            set => SetValue(UnderlineOnHighlightProperty, value);
+        }
+
+        /// <summary>
+        /// Create a LinkTextBlock.
+        /// </summary>
+        public LinkTextBlock() : base()
+        {
+            TextDecorations = null;
+            Foreground = TextBrush;
+            Focusable = true;
+
+            VerticalAlignment = VerticalAlignment.Top;
+            HorizontalAlignment = HorizontalAlignment.Left;
+
+            PointerEntered += LinkTextBlock_MouseEnter;
+            PointerExited += LinkTextBlock_MouseLeave;
+            AddHandler(PointerReleasedEvent, LinkTextBlock_PreviewTouchUp, RoutingStrategies.Tunnel);
+            GotFocus += LinkTextBlock_GotKeyboardFocus;
+            LostFocus += LinkTextBlock_LostKeyboardFocus;
+            AddHandler(KeyUpEvent, LinkTextBlock_PreviewKeyUp, RoutingStrategies.Tunnel);
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether this element is enabled in the user interface (UI).
+        /// </summary>
+        new public bool IsEnabled
+        {
+            get { return base.IsEnabled; }
+            set
+            {
+                base.IsEnabled = value;
+
+                if (value == false)
+                {
+                    Foreground = DisabledBrush;
+                }
+                else
+                {
+                    Foreground = TextBrush;
+                }
+            }
+        }
+
+        private void Highlight()
+        {
+            if (IsEnabled)
+            {
+                if (UnderlineOnHighlight)
+                {
+                    if (TextDecorations != null)
+                    {
+                        TextDecorations.Add(new TextDecoration() { Location = TextDecorationLocation.Underline });
+                    }
+                    else
+                    {
+                        TextDecorations = new TextDecorationCollection() { new TextDecoration() { Location = TextDecorationLocation.Underline } };
+                    }
+                }
+                Foreground = HighlightBrush;
+                isHighlighted = true;
+            }
+        }
+
+        private void Unlight()
+        {
+            if (IsEnabled)
+            {
+                TextDecorations = null;
+                Foreground = TextBrush;
+                isHighlighted = false;
+            }
+        }
+
+        void LinkTextBlock_MouseEnter(object? sender, PointerEventArgs e)
+        {
+            Highlight();
+        }
+
+        void LinkTextBlock_MouseLeave(object? sender, PointerEventArgs e)
+        {
+            Unlight();
+        }
+
+        void LinkTextBlock_GotKeyboardFocus(object? sender, GotFocusEventArgs e)
+        {
+            Highlight();
+        }
+
+        void LinkTextBlock_LostKeyboardFocus(object? sender, RoutedEventArgs e)
+        {
+            Unlight();
+        }
+
+        private void RaiseClick()
+        {
+            if (AutoSetLinkFromText)
+            {
+                OpenLinkFromText();
+            }
+
+            RoutedEventArgs re = new RoutedEventArgs(ClickEvent);
+            RaiseEvent(re);
+        }
+
+        void LinkTextBlock_PreviewTouchUp(object? sender, PointerReleasedEventArgs e)
+        {
+            RaiseClick();
+        }
+
+        void LinkTextBlock_PreviewKeyUp(object? sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter || e.Key == Key.Space)
+            {
+                RaiseClick();
+            }
+        }
+
+        void OpenLinkFromText()
+        {
+            try
+            {
+                if (Text == null)
+                {
+                    return;
+                }
+
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                {
+                    Process.Start(new ProcessStartInfo(Text) { UseShellExecute = true });
+                }
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                {
+                    Process.Start("xdg-open", Text);
+                }
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+                {
+                    Process.Start("open", Text);
+                }
+            }
+            catch (ArgumentNullException)
+            {
+                // could not open the link as it is null
+                if (Debugger.IsAttached)
+                {
+                    Debugger.Log(0, "LinkTextBlock", "Link is null, from LinkTextBlock " + Name + ".\n");
+                }
+            }
+            catch (System.IO.FileNotFoundException)
+            {
+                // could not open the link as it doesn't exist
+                if (Debugger.IsAttached)
+                {
+                    Debugger.Log(0, "LinkTextBlock", "Link \"" + Text + "\" does not exist, from LinkTextBlock " + Name + ".\n");
+                }
+            }
+            catch (InvalidOperationException)
+            {
+                // could not open the link for some reason
+                if (Debugger.IsAttached)
+                {
+                    Debugger.Log(0, "LinkTextBlock", "Link \"" + Text + "\" was not defined, from LinkTextBlock " + Name + ".\n");
+                }
+            }
+            catch (Win32Exception)
+            {
+                // could not open the link for some reason
+                if (Debugger.IsAttached)
+                {
+                    Debugger.Log(0, "LinkTextBlock", "Link \"" + Text + "\" could not be opened, from LinkTextBlock " + Name + ".\n");
+                }
+            }
+            //catch (PlatformNotSupportedException)
+            //{
+            //    try
+            //    {
+            //        // must use UseShellExecute so that it works on .NET Core
+            //        Process.Start(new ProcessStartInfo { FileName = Text });
+            //    }
+            //    catch (ArgumentNullException)
+            //    {
+            //        // could not open the link as it is null
+            //        if (Debugger.IsAttached)
+            //        {
+            //            Debugger.Log(0, "LinkTextBlock", "Link is null, from LinkTextBlock " + Name + ".\n");
+            //        }
+            //    }
+            //    catch (System.IO.FileNotFoundException)
+            //    {
+            //        // could not open the link as it doesn't exist
+            //        if (Debugger.IsAttached)
+            //        {
+            //            Debugger.Log(0, "LinkTextBlock", "Link \"" + Text + "\" does not exist, from LinkTextBlock " + Name + ".\n");
+            //        }
+            //    }
+            //    catch (InvalidOperationException)
+            //    {
+            //        // could not open the link for some reason
+            //        if (Debugger.IsAttached)
+            //        {
+            //            Debugger.Log(0, "LinkTextBlock", "Link \"" + Text + "\" was not defined, from LinkTextBlock " + Name + ".\n");
+            //        }
+            //    }
+            //    catch (Win32Exception)
+            //    {
+            //        // could not open the link for some reason
+            //        if (Debugger.IsAttached)
+            //        {
+            //            Debugger.Log(0, "LinkTextBlock", "Link \"" + Text + "\" could not be opened, from LinkTextBlock " + Name + ".\n");
+            //        }
+            //    }
+            //}
+        }
+
+    }
+}
