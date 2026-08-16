8b4c53b336e370decb910ed7a8f08f28f9c3998e
JaykeBird <jacobrh94@hotmail.com>
2026-08-15 23:46:47 -0500

    Create RibbonTextBlock control
    This way, users can put a TextBlock within the Ribbon, without tying it to a button or other interactive control.

---
+++ SolidShineUi/Ribbon/RibbonTextBlock.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace SolidShineUi.Ribbon
+{
+    /// <summary>
+    /// A Ribbon control that displays some text (and an icon, if desired) and cannot be interacted with.
+    /// </summary>
+    public class RibbonTextBlock : ThemedControl, IRibbonItem
+    {
+
+        /// <summary>
+        /// Get or set the size to use for this control.
+        /// </summary>
+        public RibbonElementSize StandardSize { get => (RibbonElementSize)GetValue(StandardSizeProperty); set => SetValue(StandardSizeProperty, value); }
+
+        /// <summary>The backing dependency property for <see cref="StandardSize"/>. See the related property for details.</summary>
+        public static DependencyProperty StandardSizeProperty
+            = DependencyProperty.Register("StandardSize", typeof(RibbonElementSize), typeof(RibbonTextBlock),
+            new FrameworkPropertyMetadata(RibbonElementSize.Small));
+
+        /// <summary>
+        /// Get or set the size to use for this control, when the parent group is being compacted.
+        /// </summary>
+        public RibbonElementSize CompactSize { get => (RibbonElementSize)GetValue(CompactSizeProperty); set => SetValue(CompactSizeProperty, value); }
+
+        /// <summary>The backing dependency property for <see cref="CompactSize"/>. See the related property for details.</summary>
+        public static DependencyProperty CompactSizeProperty
+            = DependencyProperty.Register("CompactSize", typeof(RibbonElementSize), typeof(RibbonTextBlock),
+            new FrameworkPropertyMetadata(RibbonElementSize.Small));
+
+        /// <inheritdoc/>
+        public string AccessKey { get => (string)GetValue(AccessKeyProperty); set => SetValue(AccessKeyProperty, value); }
+
+        /// <summary>The backing dependency property for <see cref="AccessKey"/>. See the related property for details.</summary>
+        public static DependencyProperty AccessKeyProperty
+            = DependencyProperty.Register("AccessKey", typeof(string), typeof(RibbonTextBlock),
+            new FrameworkPropertyMetadata("I"));
+
+        /// <summary>
+        /// Get or set the text to display for this control.
+        /// </summary>
+        public string Title { get => (string)GetValue(TitleProperty); set => SetValue(TitleProperty, value); }
+
+        /// <summary>The backing dependency property for <see cref="Title"/>. See the related property for details.</summary>
+        public static DependencyProperty TitleProperty
+            = DependencyProperty.Register("Title", typeof(string), typeof(RibbonTextBlock),
+            new FrameworkPropertyMetadata("Item"));
+
+        /// <summary>
+        /// Get or set if the <see cref="SmallIcon"/> should be displayed next to the control.
+        /// </summary>
+        public bool ShowIcon { get => (bool)GetValue(ShowIconProperty); set => SetValue(ShowIconProperty, value); }
+
+        /// <summary>The backing dependency property for <see cref="ShowIcon"/>. See the related property for details.</summary>
+        public static DependencyProperty ShowIconProperty
+            = DependencyProperty.Register("ShowIcon", typeof(bool), typeof(RibbonTextBlock),
+            new FrameworkPropertyMetadata(true));
+
+        /// <summary>
+        /// Get or set the large icon to use with this control. For <see cref="RibbonTextBlock"/>,
+        /// this property actually does nothing, as the <c>LargeIcon</c> is never displayed on the Ribbon. Instead, only set <see cref="SmallIcon"/>.
+        /// </summary>
+        public ImageSource LargeIcon { get => (ImageSource)GetValue(LargeIconProperty); set => SetValue(LargeIconProperty, value); }
+
+        /// <summary>The backing dependency property for <see cref="LargeIcon"/>. See the related property for details.</summary>
+        public static DependencyProperty LargeIconProperty
+            = DependencyProperty.Register("LargeIcon", typeof(ImageSource), typeof(RibbonTextBlock),
+            new FrameworkPropertyMetadata(null));
+
+        /// <summary>
+        /// Get or set the small icon to use with this control. 
+        /// This will be displayed to the left of the control (and to the left of the <see cref="Title"/>), and can be hidden via <see cref="ShowIcon"/>.
+        /// </summary>
+        /// <remarks>
+        /// The small icon should be 16x16 in size.
+        /// </remarks>
+        public ImageSource SmallIcon { get => (ImageSource)GetValue(SmallIconProperty); set => SetValue(SmallIconProperty, value); }
+
+        /// <summary>The backing dependency property for <see cref="SmallIcon"/>. See the related property for details.</summary>
+        public static DependencyProperty SmallIconProperty
+            = DependencyProperty.Register("SmallIcon", typeof(ImageSource), typeof(RibbonTextBlock),
+            new FrameworkPropertyMetadata(null));
+
+        /// <inheritdoc/>
+        public int CompactOrder { get => (int)GetValue(CompactOrderProperty); set => SetValue(CompactOrderProperty, value); }
+
+        /// <summary>The backing dependency property for <see cref="CompactOrder"/>. See the related property for details.</summary>
+        public static DependencyProperty CompactOrderProperty
+            = DependencyProperty.Register("CompactOrder", typeof(int), typeof(RibbonTextBlock),
+            new FrameworkPropertyMetadata(0));
+
+        /// <inheritdoc/>
+        public bool IsCompacted { get => (bool)GetValue(IsCompactedProperty); set => SetValue(IsCompactedProperty, value); }
+
+        /// <summary>The backing dependency property for <see cref="IsCompacted"/>. See the related property for details.</summary>
+        public static DependencyProperty IsCompactedProperty
+            = DependencyProperty.Register("IsCompacted", typeof(bool), typeof(RibbonTextBlock),
+            new FrameworkPropertyMetadata(false));
+
+        #region Color Scheme
+        /// <summary>
+        /// Raised when the ColorScheme property is changed.
+        /// </summary>
+#if NETCOREAPP
+        public event DependencyPropertyChangedEventHandler? ColorSchemeChanged;
+#else
+        public event DependencyPropertyChangedEventHandler ColorSchemeChanged;
+#endif
+
+        /// <summary>
+        /// A dependency property object backing the related ColorScheme property. See <see cref="ColorScheme"/> for more details.
+        /// </summary>
+        public static readonly DependencyProperty ColorSchemeProperty
+            = DependencyProperty.Register(nameof(ColorScheme), typeof(ColorScheme), typeof(RibbonTextBlock),
+            new FrameworkPropertyMetadata(new ColorScheme(), new PropertyChangedCallback(OnColorSchemeChanged)));
+
+        /// <summary>
+        /// Perform an action when the ColorScheme property has changed. Primarily used internally.
+        /// </summary>
+        /// <param name="d">The object containing the property that changed.</param>
+        /// <param name="e">Event arguments about the property change.</param>
+        public static void OnColorSchemeChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+#if NETCOREAPP
+            ColorScheme cs = (e.NewValue as ColorScheme)!;
+#else
+            ColorScheme cs = e.NewValue as ColorScheme;
+#endif
+
+            if (d is RibbonTextBlock c)
+            {
+                c.ColorSchemeChanged?.Invoke(d, e);
+                c.ApplyColorScheme(cs);
+            }
+        }
+
+        /// <summary>
+        /// Get or set the color scheme used for this control. The color scheme can quickly apply a whole visual style to your control.
+        /// </summary>
+        [Category("Appearance")]
+        public ColorScheme ColorScheme
+        {
+            get => (ColorScheme)GetValue(ColorSchemeProperty);
+            set => SetValue(ColorSchemeProperty, value);
+        }
+
+        /// <summary>
+        /// Apply a color scheme to this control. The color scheme can quickly apply a whole visual style to the control.
+        /// </summary>
+        /// <param name="cs">The color scheme to apply.</param>
+        public void ApplyColorScheme(ColorScheme cs)
+        {
+            if (cs == null)
+            {
+                return;
+            }
+            if (cs != ColorScheme)
+            {
+                ColorScheme = cs;
+                return;
+            }
+
+            Foreground = cs.ForegroundColor.ToBrush();
+        }
+
+        #endregion
+
+        /// <inheritdoc/>
+        protected override void OnApplySsuiTheme(SsuiTheme ssuiTheme, bool useLightBorder = false, bool useAccentTheme = false)
+        {
+            base.OnApplySsuiTheme(ssuiTheme, useLightBorder, useAccentTheme);
+
+            if (useAccentTheme && ssuiTheme is SsuiAppTheme ssuiAppTheme && ssuiAppTheme.AccentTheme != null)
+            {
+                ApplyTheme(ssuiAppTheme.AccentTheme);
+            }
+            else
+            {
+                ApplyTheme(ssuiTheme);
+            }
+
+            void ApplyTheme(SsuiTheme theme)
+            {
+                ApplyThemeBinding(ForegroundProperty, SsuiTheme.ForegroundProperty, theme);
+            }
+        }
+    }
+}
